09f343f85ae9afceaa32f6694df61f026ece933d
mcdoodle97 <mseamons5@hotmail.com>
2023-07-12 18:04:01 -0700

    Major blog changes

--- app/home/blogs/[slug]/page.tsx
+++ app/home/blogs/[slug]/page.tsx
@@ -1,16 +1,33 @@
 "use client";
+import React from "react";
 import { useParams } from "next/navigation";
 import { blogs } from "@/components/blogs/blogData";
 import { Blog } from "@/components/blogs/types";
 import HomeLayout from "@/components/home/HomeLayout";
 import Meta from "@/components/meta";
-import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { Section } from "@/components/home/Section";
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 const BlogDetail: React.FC = () => {
   const params = useParams();
   const { slug } = params;
-
+  const [randomBlogs, setRandomBlogs] = useState([]);
   const blog: Blog | undefined = blogs.find((blog) => blog.slug === slug);
 
+  useEffect(() => {
+    const shuffledBlogs = shuffleArray(blogs);
+    setRandomBlogs(shuffledBlogs.slice(0, 4));
+  }, []);
 
   if (!blog) {
     return (
@@ -22,13 +39,36 @@ const BlogDetail: React.FC = () => {
 
   return (
     <HomeLayout>
-      <Meta title=" test" description="test" keywords="test" />
-      <div className="flex flex-col items-center space-y-4 p-4">
-        <h1 className="mb-4 text-center text-4xl font-bold">{blog.title}</h1>
-        <img src={blog.image.path} alt={blog.image.altText} className="mb-4" />
-        <p className="text-center">{blog.content}</p>
+      <Section title={blog.title} description={blog.description} >
+      <div className="container mx-auto px-8 py-4">
+        
+        <Image
+          src={blog.image.path}
+          alt={blog.image.altText}
+          width={52}
+          height={52}
+          className="max-w-lg w-full h-auto mx-auto"
+        />
+        <div className="max-w-3xl mx-auto mt-8">
+          <p className="text-lg text-center text-gray-700  ">{blog.content}</p>
+        </div>
       </div>
+      <hr className="border-t-2 border-gray-200 my-8" />
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-8">
+        {randomBlogs.map((blog, index) => (
+          <div key={index} className="flex flex-col items-center justify-center space-y-4 p-4">
+            <Image src={blog.image.path} alt={blog.image.altText} width={52} height={52} />
+            <h2 className="text-2xl font-semibold text-center">{blog.title}</h2>
+            <p className="text-lg text-center text-gray-700">{blog.description}</p>
+            <Link legacyBehavior href={`/blogs/${blog.slug}`}>
+              <a className="text-blue-600 hover:text-blue-800">Read More &#8599;</a>
+            </Link>
+          </div>
+        ))}
+      </div>
+      </Section>
     </HomeLayout>
+    
   );
 };
 
