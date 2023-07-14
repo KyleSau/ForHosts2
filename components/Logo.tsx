946e3b30a31307bf19d544d86e5f072116daa1a4
ssssylassss <125253203+ssssylassss@users.noreply.github.com>
2023-07-14 17:15:45 -0500

    added slugless logo, changed directory, delete old

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 
 function Logo() {
-  return <Image src="/ForHostsLogo.svg" alt="Logo" width={160} height={50} />
+  return <Image src="/ForHostsLogoWithoutSlug.svg" alt="Logo" width={160} height={50} />
 }
 
 export default Logo;
