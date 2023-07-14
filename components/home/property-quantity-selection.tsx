c8fccee55bfac366c2f78cc38e848caa4ee3ec0e
mcdoodle97 <mseamons5@hotmail.com>
2023-07-14 16:15:16 -0700

    fixed cursor

--- components/home/property-quantity-selection.tsx
+++ components/home/property-quantity-selection.tsx
@@ -12,57 +12,44 @@ const PropertyQuantitySelection = () => {
         title="Tell us a bit about yourself..."
         description="Please select how many properties you need listings for"
       >
-        <div className="grid md:grid-cols-2 gap-4 items-center justify-items-center">
-          <div 
+        <div className="grid items-center justify-items-center gap-4 md:grid-cols-2">
+          <div
             className={`flex flex-col items-center transition-all`}
             onMouseEnter={() => setIsHovered1(true)}
             onMouseLeave={() => setIsHovered1(false)}
           >
-            <div
-              className="m-2  rounded-full border-4 border-gray-600 p-16 w-64 h-64 flex items-center justify-center relative"
-              onClick={() => console.log("Card 1 clicked")}
-            >
-              <div className="text-center flex flex-col items-center justify-center relative z-10">
+            <div className="relative  m-2 flex h-64 w-64 items-center justify-center rounded-full border-4 border-gray-600 p-16">
+              <div className="relative z-10 flex flex-col items-center justify-center text-center">
                 <img
-                  className="object-contain w-full h-32"
+                  className="h-32 w-full object-contain"
                   src="/seeThroughHouse.svg"
                   alt="SVG for Card 1"
                 />
                 <p className="mt-2">I manage 1-10 properties</p>
               </div>
             </div>
-            
-           
 
-<button className="hover:text-black text-black hover:bg-sitecolor hover:scale-110 transition ease-in-out delay-75 font-bold py-2 px-4 rounded mt-3 border border-black ">
-  <Link href="/get-started">
-    Get Started &#8599;
-  </Link>
-</button>
-
+            <button className="mt-3 rounded border border-black px-4 py-2 font-bold text-black transition delay-75 ease-in-out hover:scale-110 hover:bg-sitecolor hover:text-black ">
+              <Link href="/get-started">Get Started &#8599;</Link>
+            </button>
           </div>
-          <div 
+          <div
             className={`flex flex-col items-center transition-all`}
             onMouseEnter={() => setIsHovered2(true)}
             onMouseLeave={() => setIsHovered2(false)}
           >
-            <div
-              className="m-2 cursor-pointer rounded-full border-4 border-gray-600 p-16 w-64 h-64 flex items-center justify-center relative"
-              onClick={() => console.log("Card 2 clicked")}
-            >
-              <div className="text-center flex flex-col items-center justify-center relative z-10">
+            <div className="relative m-2 flex h-64 w-64 items-center justify-center rounded-full border-4 border-gray-600 p-16">
+              <div className="relative z-10 flex flex-col items-center justify-center text-center">
                 <img
-                  className="object-contain w-full h-32"
+                  className="h-32 w-full object-contain"
                   src="/bigHome.svg"
                   alt="SVG for Card 2"
                 />
                 <p className="mt-2">I manage 10+ properties</p>
               </div>
             </div>
-            <button className="hover:text-black text-black hover:bg-sitecolor hover:scale-110 transition ease-in-out delay-75 font-bold py-2 px-4 rounded mt-3 border border-black ">
-            <Link href="/get-started">
-    Get Started &#8599;
-  </Link>
+            <button className="mt-3 rounded border border-black px-4 py-2 font-bold text-black transition delay-75 ease-in-out hover:scale-110 hover:bg-sitecolor hover:text-black ">
+              <Link href="/get-started">Get Started &#8599;</Link>
             </button>
           </div>
         </div>
