7e099de2e457a727300ccc6207835fcdda3eee16
ssssylassss <125253203+ssssylassss@users.noreply.github.com>
2023-07-14 16:29:24 -0500

    import fixes

--- app/home/pricing/page.tsx
+++ app/home/pricing/page.tsx
@@ -1,12 +1,12 @@
 import HomeLayout from "@/components/home/home-layout";
 import Navbar from "@/components/home/nav-bar";
-import PriceCards from "@/components/home/PricingCard";
+import PriceCards from "@/components/home/pricing-card";
 import Meta from "@/components/meta";
 export default function Pricing() {
   return (
     <HomeLayout>
-  <Meta title="ForHosts pricing for a dynamic website" description="Pricing packages for property hosts to make their own dynamic websites!" />
-<PriceCards />
-   </HomeLayout>
+      <Meta title="ForHosts pricing for a dynamic website" description="Pricing packages for property hosts to make their own dynamic websites!" />
+      <PriceCards />
+    </HomeLayout>
   );
 }
