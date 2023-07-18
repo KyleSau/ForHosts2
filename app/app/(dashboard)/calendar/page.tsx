0cd236d8010d1c793610c5b6dfd2e7e928e00f9d
ssssylassss <125253203+ssssylassss@users.noreply.github.com>
2023-07-17 20:34:36 -0500

    big calendar

--- app/app/(dashboard)/calendar/page.tsx
+++ app/app/(dashboard)/calendar/page.tsx
@@ -1,22 +1,76 @@
-import { ReactNode } from "react";
-import Form from "@/components/form";
-import { getSession } from "@/lib/auth";
-import { redirect } from "next/navigation";
-import { editUser } from "@/lib/actions";
+"use client"
+import {
+    Calendar as BigCalendar,
+    momentLocalizer,
+    Views
+} from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
-export default async function CalendarPage() {
-    const session = await getSession();
-    if (!session) {
-        redirect("/login");
+moment.locale("en-GB");
+const localizer = momentLocalizer(moment);
+
+const events = [
+    {
+        id: 0,
+        title: "Board meeting",
+        start: new Date(2018, 0, 29, 9, 0, 0),
+        end: new Date(2018, 0, 29, 13, 0, 0),
+        resourceId: 1
+    },
+    {
+        id: 1,
+        title: "MS training",
+        allDay: true,
+        start: new Date(2018, 0, 29, 14, 0, 0),
+        end: new Date(2018, 0, 29, 16, 30, 0),
+        resourceId: 2
+    },
+    {
+        id: 2,
+        title: "Team lead meeting",
+        start: new Date(2018, 0, 29, 8, 30, 0),
+        end: new Date(2018, 0, 29, 12, 30, 0),
+        resourceId: 3
+    },
+    {
+        id: 11,
+        title: "Birthday Party",
+        start: new Date(2018, 0, 30, 7, 0, 0),
+        end: new Date(2018, 0, 30, 10, 30, 0),
+        resourceId: 4
     }
+];
+
+const resourceMap = [
+    { resourceId: 1, resourceTitle: "Board room" },
+    { resourceId: 2, resourceTitle: "Training room" },
+    { resourceId: 3, resourceTitle: "Meeting room 1" },
+    { resourceId: 4, resourceTitle: "Meeting room 2" }
+];
+
+const styles = {
+    container: {
+        width: "80wh",
+        height: "60vh",
+        margin: "2em"
+    }
+};
+
+export default function CustomCalendar() {
     return (
-        <div className="flex max-w-screen-xl flex-col space-y-12 p-8">
-            <div className="flex flex-col space-y-6">
-                <h1 className="font-cal text-3xl font-bold dark:text-white">
-                    Calendar View
-                </h1>
-                This page is where hosts Calendar would be displayed.
-            </div>
+        <div style={styles.container}>
+            <BigCalendar
+                selectable
+                localizer={localizer}
+                events={events}
+                defaultView={Views.DAY}
+                views={[Views.DAY, Views.WEEK, Views.MONTH]}
+                step={60}
+                resources={resourceMap}
+                resourceIdAccessor="resourceId"
+                resourceTitleAccessor="resourceTitle"
+            />
         </div>
     );
 }
