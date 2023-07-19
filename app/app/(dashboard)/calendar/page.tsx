be3464f47d40e819f29821d6280e2d1a5637dca1
ssssylassss <125253203+ssssylassss@users.noreply.github.com>
2023-07-18 22:26:30 -0500

    calendar works kinda

--- app/app/(dashboard)/calendar/page.tsx
+++ app/app/(dashboard)/calendar/page.tsx
@@ -6,34 +6,78 @@ import {
 } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import { title } from "process";
-import { color } from "framer-motion";
+import { useState, useEffect } from "react";
+import { getReservationFields, getReservations } from "@/lib/actions";
 
 moment.locale("en-GB");
 const localizer = momentLocalizer(moment);
 
-const events = [
+enum EventTypes {
+    PREVIOUS = "previous",
+    ACTIVE = "active", // ongoing and future
+    BLOCKED = "blocked"
+}
+
+// abstract this with interface
+const getEventStatus = (start: Date, end: Date, blocked: boolean): string => {
+    const now = moment();
+    const endDate = moment(end);
+
+    if (blocked) {
+        return EventTypes.BLOCKED;
+    }
+
+    if (now.isAfter(endDate)) {
+        return EventTypes.PREVIOUS;
+    } else {
+        return EventTypes.ACTIVE;
+    }
+}
+
+interface Event {
+    id: number;
+    title: string;
+    start: Date;
+    end: Date;
+    blocked: boolean;
+}
+
+const events: Event[] = [
     {
         id: 1,
         title: "booked",
         start: new Date(2023, 6, 8, 8, 30, 0),
         end: new Date(2023, 6, 23, 12, 30, 0),
-        resourceId: 3
+        blocked: true
     },
     {
         id: 2,
         title: "booked2",
-        allDay: true,
+        // allDay: true,
         start: new Date(2023, 6, 8, 8, 30, 0),
         end: new Date(2023, 6, 16, 12, 30, 0),
-        resourceId: 2
+        blocked: false
     },
     {
         id: 3,
         title: "booked3",
         start: new Date(2023, 6, 8, 8, 30, 0),
         end: new Date(2023, 6, 11, 12, 30, 0),
-        resourceId: 3
+        blocked: false
+    },
+    {
+        id: 4,
+        title: "booked22",
+        start: new Date(2023, 6, 9, 8, 30, 0),
+        end: new Date(2023, 6, 15, 12, 30, 0),
+        blocked: false
+    },
+    {
+        id: 5,
+        title: "noneblocked",
+        start: new Date(2023, 6, 25, 8, 30, 0),
+        end: new Date(2023, 6, 27, 12, 30, 0),
+        blocked: false
     },
 ];
 
@@ -41,7 +85,8 @@ const resourceMap = [
     { resourceId: 1, resourceTitle: "Rental 1" },
     { resourceId: 2, resourceTitle: "Rental 2" },
     { resourceId: 3, resourceTitle: "Rental 3" },
-    { resourceId: 4, resourceTitle: "Rental 4" }
+    { resourceId: 4, resourceTitle: "Rental 4" },
+    { resourceId: 5, resourceTitle: "Rental 4" }
 ];
 
 const styles = {
@@ -52,12 +97,81 @@ const styles = {
     }
 };
 
+
+const components = {
+    event: (props: any) => {
+        const { event } = props;
+        switch (getEventStatus(event.start, event.end, event.blocked)) {
+            case "previous":
+                return <div style={{ background: "gray" }}>{event.title}</div>;
+            case "active":
+                return <div style={{ background: "green" }}>{event.title}</div>;
+            case "blocked":
+                return <div style={{ background: "red" }}>{event.title}</div>;
+            default:
+                return null;
+        }
+    }
+}
+
+interface Reservation {
+    [key: string]: any;
+}
+
+
 export default function CustomCalendar() {
+
+    const [events, setEvents] = useState<Event[]>([]);
+
+    // const [reservationFields, setReservationFields] = useState<Prisma.DMMF.Field[] | undefined>(undefined);
+    const [reservations, setReservations] = useState<any>([]);
+
+    useEffect(() => {
+        async function handleGetReservationFields() {
+            const resFields = await getReservationFields();
+            // setReservationFields(resFields);
+        }
+        handleGetReservationFields();
+    }, []);
+
+    // do a join in actions.ts to get title in reservation result set.
+    useEffect(() => {
+        async function handleGetReservations(): Promise<void> {
+            const reservations = await getReservations();
+            setReservations(reservations);
+            const events: Event[] = [];
+            if (Array.isArray(reservations)) {
+                let idx = 0;
+                for (const reservation of reservations) {
+                    Object.entries(reservation).forEach(([key, value]) => {
+                        console.log(`${key}: ${value}`);
+                    });
+                    events.push({
+                        id: idx++,
+                        title: reservation.listingId,
+                        start: new Date(reservation.startDate),
+                        end: new Date(reservation.endDate),
+                        blocked: false
+                    }); // clk8uat0l0002jt08satnq0dd 
+                }
+                setEvents(events);
+            } else {
+                console.log(reservations.error);
+            }
+        }
+
+        handleGetReservations();
+    }, []);
+
+
+
+
     return (
         <div className="flex max-w-screen-xl flex-col">
             <div className="flex flex-col">
                 <div style={styles.container}>
                     <BigCalendar
+                        components={components}
                         selectable
                         localizer={localizer}
                         events={events}
--- components/reservations/reservation-table.tsx
+++ components/reservations/reservation-table.tsx
@@ -20,18 +20,18 @@ const ReservationTable: React.FC<Props> = () => {
 
   useEffect(() => {
     async function handleGetReservationFields() {
-        const resFields = await getReservationFields();
-        setReservationFields(resFields);
+      const resFields = await getReservationFields();
+      setReservationFields(resFields);
     }
     handleGetReservationFields();
   }, []);
 
   useEffect(() => {
-      async function handleGetReservations(): Promise<any> {
-          const reservations = await getReservations();
-          setReservations(reservations);
-      }
-      handleGetReservations();
+    async function handleGetReservations(): Promise<any> {
+      const reservations = await getReservations();
+      setReservations(reservations);
+    }
+    handleGetReservations();
   }, []);
 
   return (
@@ -39,7 +39,7 @@ const ReservationTable: React.FC<Props> = () => {
       <table className="min-w-full divide-y divide-gray-200 border-collapse lg:w-auto">
         <thead className="bg-gray-50">
           <tr>
-            { reservationFields && 
+            {reservationFields &&
               <th
                 scope="col"
                 className="px-2 sm:px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider border-r"
@@ -47,7 +47,7 @@ const ReservationTable: React.FC<Props> = () => {
                 #
               </th>
             }
-            { reservationFields?.map((field, fieldIdx) => (
+            {reservationFields?.map((field, fieldIdx) => (
               <th
                 scope="col"
                 key={fieldIdx}
@@ -58,25 +58,25 @@ const ReservationTable: React.FC<Props> = () => {
             ))}
           </tr>
         </thead>
-          <tbody className="text-white divide-y divide-gray-200">
-            { reservations.map((reservation: Reservation, idx: number) => {
-              return (
-                <tr className="hover:bg-gray-500" key={idx}>
-                  <td className="px-2 sm:px-6 py-4 text-center border-r">{idx+1}</td>
-                  { reservationFields?.map((field, fieldIdx) => {
-                    type ObjectKey = keyof typeof reservation;
-                    const fieldName = field.name as ObjectKey;
-                    const fieldValue = reservation[fieldName];
-                    return (
-                      <td key={fieldIdx} className="px-2 sm:px-6 py-4 text-center border-r">
-                        {fieldValue instanceof Date? fieldValue.toUTCString(): fieldValue}
-                      </td>
-                    );
-                  })}
-                </tr>
-              );
-            })}
-          </tbody>
+        <tbody className="text-white divide-y divide-gray-200">
+          {reservations.map((reservation: Reservation, idx: number) => {
+            return (
+              <tr className="hover:bg-gray-500" key={idx}>
+                <td className="px-2 sm:px-6 py-4 text-center border-r">{idx + 1}</td>
+                {reservationFields?.map((field, fieldIdx) => {
+                  type ObjectKey = keyof typeof reservation;
+                  const fieldName = field.name as ObjectKey;
+                  const fieldValue = reservation[fieldName];
+                  return (
+                    <td key={fieldIdx} className="px-2 sm:px-6 py-4 text-center border-r">
+                      {fieldValue instanceof Date ? fieldValue.toUTCString() : fieldValue}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </div>
   );
