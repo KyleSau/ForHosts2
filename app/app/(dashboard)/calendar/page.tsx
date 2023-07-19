e0a412b8503f31342a7301ad0769532c0820a740
ssssylassss <125253203+ssssylassss@users.noreply.github.com>
2023-07-18 19:28:08 -0500

    calendar styling

--- app/app/(dashboard)/calendar/page.tsx
+++ app/app/(dashboard)/calendar/page.tsx
@@ -6,71 +6,71 @@ import {
 } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import { title } from "process";
+import { color } from "framer-motion";
 
 moment.locale("en-GB");
 const localizer = momentLocalizer(moment);
 
 const events = [
     {
-        id: 0,
-        title: "Board meeting",
-        start: new Date(2018, 0, 29, 9, 0, 0),
-        end: new Date(2018, 0, 29, 13, 0, 0),
-        resourceId: 1
-    },
-    {
         id: 1,
-        title: "MS training",
-        allDay: true,
-        start: new Date(2018, 0, 29, 14, 0, 0),
-        end: new Date(2018, 0, 29, 16, 30, 0),
-        resourceId: 2
-    },
-    {
-        id: 2,
-        title: "Team lead meeting",
-        start: new Date(2018, 0, 29, 8, 30, 0),
-        end: new Date(2018, 0, 29, 12, 30, 0),
+        title: "booked",
+        start: new Date(2023, 6, 8, 8, 30, 0),
+        end: new Date(2023, 6, 23, 12, 30, 0),
         resourceId: 3
     },
     {
-        id: 11,
-        title: "Birthday Party",
-        start: new Date(2018, 0, 30, 7, 0, 0),
-        end: new Date(2018, 0, 30, 10, 30, 0),
-        resourceId: 4
-    }
+        id: 2,
+        title: "booked2",
+        allDay: true,
+        start: new Date(2023, 6, 8, 8, 30, 0),
+        end: new Date(2023, 6, 16, 12, 30, 0),
+        resourceId: 2
+    },
+    {
+        id: 3,
+        title: "booked3",
+        start: new Date(2023, 6, 8, 8, 30, 0),
+        end: new Date(2023, 6, 11, 12, 30, 0),
+        resourceId: 3
+    },
 ];
 
 const resourceMap = [
-    { resourceId: 1, resourceTitle: "Board room" },
-    { resourceId: 2, resourceTitle: "Training room" },
-    { resourceId: 3, resourceTitle: "Meeting room 1" },
-    { resourceId: 4, resourceTitle: "Meeting room 2" }
+    { resourceId: 1, resourceTitle: "Rental 1" },
+    { resourceId: 2, resourceTitle: "Rental 2" },
+    { resourceId: 3, resourceTitle: "Rental 3" },
+    { resourceId: 4, resourceTitle: "Rental 4" }
 ];
 
 const styles = {
     container: {
-        width: "80wh",
-        height: "60vh",
-        margin: "2em"
+        width: "100wh",
+        height: "100vh",
+        margin: "ml-auto mr-auto"
     }
 };
 
 export default function CustomCalendar() {
     return (
-        <div style={styles.container}>
-            <BigCalendar
-                selectable
-                localizer={localizer}
-                events={events}
-                defaultView={Views.DAY}
-                views={[Views.DAY, Views.WEEK, Views.MONTH]}
-                step={60}
-                resources={resourceMap}
-                resourceIdAccessor="resourceId"
-                resourceTitleAccessor="resourceTitle"
-            />
+        <div className="flex max-w-screen-xl flex-col">
+            <div className="flex flex-col">
+                <div style={styles.container}>
+                    <BigCalendar
+                        selectable
+                        localizer={localizer}
+                        events={events}
+                        defaultView={Views.MONTH}
+                        views={[Views.DAY, Views.WEEK, Views.MONTH]}
+                        step={60}
+                        resources={resourceMap}
+                        resourceIdAccessor="resourceId"
+                        resourceTitleAccessor="resourceTitle"
+                    />
+                </div>
+            </div>
         </div>
+
     );
 }
