5dec8ee2ed1e8ba57de3d2f7d13ea936b7b2d387
KWSaunders <kwsaunders@pipeline.sbcc.edu>
2023-07-20 15:48:19 -0700

    hard rington

--- app/api/domain/[slug]/calendar/route.ts
+++ app/api/domain/[slug]/calendar/route.ts
@@ -17,40 +17,40 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     // Generate the content you want to return for the calendar without the HTML and metadata
     // const calendarContent = JSON.stringify(events);
     const calendarContent = `
-        BEGIN: VCALENDAR
-        VERSION: 2.0
-        PRODID: -//Your Company//Your Application//EN
-        CALSCALE: GREGORIAN
-        BEGIN: VEVENT
-        SUMMARY: clkalnvrt0002mi084cw8m8d0
-        DTSTART: 20230703T193000.000ZZ
-        DTEND: 20230727T114500.000ZZ
-        DESCRIPTION: CONFIRMED
-        LOCATION: none
-        END: VEVENT
-        BEGIN: VEVENT
-        SUMMARY: clkalnvrt0002mi084cw8m8d0
-        DTSTART: 20230703T193000.000ZZ
-        DTEND: 20230812T114500.000ZZ
-        DESCRIPTION: CONFIRMED
-        LOCATION: none
-        END: VEVENT
-        BEGIN: VEVENT
-        SUMMARY: clkalnvrt0002mi084cw8m8d0
-        DTSTART: 20230703T193000.000ZZ
-        DTEND: 20230812T114500.000ZZ
-        DESCRIPTION: CONFIRMED
-        LOCATION: none
-        END: VEVENT
-        BEGIN: VEVENT
-        SUMMARY: clkalnvrt0002mi084cw8m8d0
-        DTSTART: 20230703T193000.000ZZ
-        DTEND: 20230812T114500.000ZZ
-        DESCRIPTION: CONFIRMED
-        LOCATION: none
-        END: VEVENT
-        END: VCALENDAR
-    `;
+      BEGIN: VCALENDAR
+      VERSION: 2.0
+      PRODID: -//Your Company//Your Application//EN
+      CALSCALE: GREGORIAN
+      BEGIN: VEVENT
+      SUMMARY: clkalnvrt0002mi084cw8m8d0
+      DTSTART: 20230703T193000.000ZZ
+      DTEND: 20230727T114500.000ZZ
+      DESCRIPTION: CONFIRMED
+      LOCATION: none
+      END: VEVENT
+      BEGIN: VEVENT
+      SUMMARY: clkalnvrt0002mi084cw8m8d0
+      DTSTART: 20230703T193000.000ZZ
+      DTEND: 20230812T114500.000ZZ
+      DESCRIPTION: CONFIRMED
+      LOCATION: none
+      END: VEVENT
+      BEGIN: VEVENT
+      SUMMARY: clkalnvrt0002mi084cw8m8d0
+      DTSTART: 20230703T193000.000ZZ
+      DTEND: 20230812T114500.000ZZ
+      DESCRIPTION: CONFIRMED
+      LOCATION: none
+      END: VEVENT
+      BEGIN: VEVENT
+      SUMMARY: clkalnvrt0002mi084cw8m8d0
+      DTSTART: 20230703T193000.000ZZ
+      DTEND: 20230812T114500.000ZZ
+      DESCRIPTION: CONFIRMED
+      LOCATION: none
+      END: VEVENT
+      END: VCALENDAR
+  `;
 
     // Set the response content type to application/json
     res.setHeader('Content-Type', 'application/json');
