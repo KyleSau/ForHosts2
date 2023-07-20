0dca705a313c0547217bd9fa1aad5efc7c8ad074
Kyle Saunders <kwsaunders@pipeline.sbcc.edu>
2023-07-20 03:24:15 -0700

    ical finished

--- app/[domain]/[slug]/calendar/page.tsx
+++ app/[domain]/[slug]/calendar/page.tsx
@@ -15,12 +15,12 @@ export default async function CalendarICS({
         notFound();
     }
 
-    const reservations: any = await getReservationsByPostId(data.id);
+    const events: any = await getReservationsByPostId(data.id);
 
     return (
         <div>
             Post Id: {data.id}
-            {/* <InternetCalendarScheduler events={data} /> */}
+            <InternetCalendarScheduler events={events} />
         </div>
     );
 
--- components/util/internet-calendar-scheduler.tsx
+++ components/util/internet-calendar-scheduler.tsx
@@ -1,9 +1,55 @@
-export default function InternetCalendarScheduler({ events }: any) {
+import React from 'react';
+
+interface Event {
+    id: string;
+    postId: string;
+    startDate: Date;
+    endDate: Date;
+    totalPrice: number;
+    createdAt: Date;
+    updatedAt: Date;
+    status: string;
+    // TODO:
+    // summary
+    // description
+    // location
+}
+
+interface InternetCalendarSchedulerProps {
+    events: Event[];
+}
+
+const InternetCalendarScheduler: React.FC<InternetCalendarSchedulerProps> = ({ events }) => {
+    const generateICal = () => {
+        const lines = [
+            'BEGIN:VCALENDAR',
+            'VERSION:2.0',
+            'PRODID:-//Your Company//Your Application//EN',
+            'CALSCALE:GREGORIAN',
+        ];
+
+        events.forEach((event) => {
+            lines.push('BEGIN:VEVENT');
+            lines.push(`SUMMARY:${event.postId || 'none'}`);
+            lines.push(`DTSTART:${event.startDate.toISOString().replace(/[-:]/g, '')}Z`);
+            lines.push(`DTEND:${event.endDate.toISOString().replace(/[-:]/g, '')}Z`);
+            lines.push(`DESCRIPTION:${event.status || 'none'}`);
+            lines.push(`LOCATION:none}`);
+            lines.push('END:VEVENT');
+        });
+
+        lines.push('END:VCALENDAR');
+
+        return lines.join('\n');
+    };
+
+    const iCalData = generateICal();
 
     return (
         <div>
-            Post Id: { }
+            <pre>{iCalData}</pre>
         </div>
-    )
+    );
+};
 
-}
+export default InternetCalendarScheduler;
