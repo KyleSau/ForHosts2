32b891f87058b38f4191d5e1bd11553ec50b24ea
Kyle Saunders <kwsaunders@pipeline.sbcc.edu>
2023-07-20 03:17:32 -0700

    ical get post id

--- components/util/internet-calendar-scheduler.tsx
+++ components/util/internet-calendar-scheduler.tsx
@@ -1,62 +1,9 @@
-// interface Event {
-//     id: string;
-//     dtstart: string;
-//     dtend: string;
-//     location: string;
-//     description: string;
-//     url: string;
-// }
-
-import { GetServerSideProps } from 'next';
-import { ParsedUrlQuery } from 'querystring';
-import { PrismaClient } from '@prisma/client';
-
-interface PathProps extends ParsedUrlQuery {
-    site: string;
-    slug: string;
-}
-
-interface PathProps extends ParsedUrlQuery {
-    site: string;
-    slug: string;
-}
-
-
 export default function InternetCalendarScheduler({ events }: any) {
 
     return (
         <div>
-
+            Post Id: { }
         </div>
     )
 
 }
-
-export const getServerSideProps: GetServerSideProps = async ({ query }) => {
-    const prisma = new PrismaClient();
-    const { site, slug } = query as PathProps;
-
-    const post = await prisma.post.findFirst({
-        where: {
-            site: {
-                subdomain: site,
-            },
-            slug,
-        },
-        select: {
-            id: true,
-        },
-    });
-
-    if (!post) {
-        return {
-            notFound: true,
-        };
-    }
-
-    return {
-        props: {
-            postId: post.id,
-        },
-    };
-};
