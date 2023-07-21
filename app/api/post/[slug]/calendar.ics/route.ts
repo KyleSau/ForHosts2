bea5c4d2f56302982f3a57e94488e83235a887ad
KWSaunders <kwsaunders@pipeline.sbcc.edu>
2023-07-20 17:09:13 -0700

    site name in ical

--- app/api/post/[slug]/calendar.ics/route.ts
+++ app/api/post/[slug]/calendar.ics/route.ts
@@ -1,17 +1,17 @@
 import { getReservationsByPostId } from '@/lib/actions';
 
 export async function GET(
-    _req: Request,
-    { params }: { params: { slug: string } },
+  _req: Request,
+  { params }: { params: { slug: string } },
 ) {
-    const postId = params.slug;
+  const postId = params.slug;
 
-    const events: any = await getReservationsByPostId(postId);
+  const events: any = await getReservationsByPostId(postId);
 
-    const calendarContent = `
+  const calendarContent = `
     BEGIN:VCALENDAR
     VERSION:2.0
-    PRODID:-//Your Company//Your Application//EN
+    PRODID:-//ForHosts.com//Host Site//EN
     CALSCALE:GREGORIAN
     ${events.map((event: any) => `
       BEGIN:VEVENT
@@ -24,63 +24,12 @@ export async function GET(
     END:VCALENDAR
   `;
 
-    const headers = new Headers();
-    headers.set('Content-Type', 'text/calendar');
-    headers.set('Content-Disposition', 'attachment; filename=calendar.ics');
+  const headers = new Headers();
+  headers.set('Content-Type', 'text/calendar');
+  headers.set('Content-Disposition', 'attachment; filename=calendar.ics');
 
-    return new Response(calendarContent, {
-        status: 200,
-        headers,
-    });
+  return new Response(calendarContent, {
+    status: 200,
+    headers,
+  });
 }
-
-
-// import { NextResponse } from "next/server";
-// import { getReservationsByPostId } from '@/lib/actions';
-
-// export async function GET(
-//     _req: Request,
-//     { params }: { params: { slug: string } },
-// ) {
-//     const postId = params.slug;
-
-//     const events: any = await getReservationsByPostId(postId);
-
-//     const calendarContent = `
-//       BEGIN: VCALENDAR
-//       VERSION: 2.0
-//       PRODID: -//Your Company//Your Application//EN
-//       CALSCALE: GREGORIAN
-//       BEGIN: VEVENT
-//       SUMMARY: clkalnvrt0002mi084cw8m8d0
-//       DTSTART: 20230703T193000.000ZZ
-//       DTEND: 20230727T114500.000ZZ
-//       DESCRIPTION: CONFIRMED
-//       LOCATION: none
-//       END: VEVENT
-//       BEGIN: VEVENT
-//       SUMMARY: clkalnvrt0002mi084cw8m8d0
-//       DTSTART: 20230703T193000.000ZZ
-//       DTEND: 20230812T114500.000ZZ
-//       DESCRIPTION: CONFIRMED
-//       LOCATION: none
-//       END: VEVENT
-//       BEGIN: VEVENT
-//       SUMMARY: clkalnvrt0002mi084cw8m8d0
-//       DTSTART: 20230703T193000.000ZZ
-//       DTEND: 20230812T114500.000ZZ
-//       DESCRIPTION: CONFIRMED
-//       LOCATION: none
-//       END: VEVENT
-//       BEGIN: VEVENT
-//       SUMMARY: clkalnvrt0002mi084cw8m8d0
-//       DTSTART: 20230703T193000.000ZZ
-//       DTEND: 20230812T114500.000ZZ
-//       DESCRIPTION: CONFIRMED
-//       LOCATION: none
-//       END: VEVENT
-//       END: VCALENDAR
-//   `;
-
-//     return NextResponse.json({ response: "ok" });
-// }
