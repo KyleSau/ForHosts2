9e03382ea136b7bc1b8244427380cc7e6f1e9220
ssssylassss <125253203+ssssylassss@users.noreply.github.com>
2023-07-23 01:17:14 -0500

    fix

--- app/[domain]/[slug]/page.tsx
+++ app/[domain]/[slug]/page.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { notFound } from "next/navigation";
 import { getPostData } from "@/lib/fetchers";
 import BlogCard from "@/components/blog-card";
@@ -31,9 +30,9 @@ import {
   faBicycle,
   faFire,
   faShieldAlt,
-  faConciergeBell,
-  // Add other icons here...
+  faConciergeBell
 } from "@fortawesome/free-solid-svg-icons";
+
 
 export async function generateMetadata({
   params,
@@ -63,7 +62,7 @@ export async function generateMetadata({
   };
 }
 
-const amenitiesList: string[] = [
+const amenitiesList: any = [
   "Wifi",
   "Kitchen Facilities",
   "Heating and Air Conditioning",
@@ -87,7 +86,7 @@ const amenitiesList: string[] = [
   "Bikes",
   "Fireplace",
   "Security Features",
-  "Concierge Services",
+  "Concierge Services"
 ];
 
 const amenityDetails = {
@@ -189,11 +188,333 @@ const amenityDetails = {
   }
 };
 
+const amenitiesMap: any = amenitiesList.reduce((map: any, amenity: any) => {
+  map[amenity] = amenityDetails.amenity || { icon: "default-icon", description: "" };
+  return map;
+}, {});
+
+export default async function SitePostPage({
+  params,
+}: {
+  params: { domain: string; slug: string };
+}) {
+  const { domain, slug } = params;
+  const data = await getPostData(domain, slug);
+
+  const amenities = [
+    'Towels',
+    'Wifi',
+    'Bathroom',
+    'Breakfast'
+  ];
+
+  if (!data) {
+    notFound();
+  }
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center">
+        <div className="m-auto w-full text-center md:w-7/12">
+          <p className="m-auto my-5 w-10/12 text-sm font-light text-stone-500 dark:text-stone-400 md:text-base">
+            {toDateString(data.createdAt)}
+          </p>
+          {/* Prop for postId */}
+          <ReservationForm postId={data.id} />
+          <h1 className="mb-10 font-title text-3xl font-bold text-stone-800 dark:text-white md:text-6xl">
+            {data.title}
+          </h1>
+          <p className="text-md m-auto w-10/12 text-stone-600 dark:text-stone-400 md:text-lg">
+            {data.description}
+          </p>
+          <h2>Amenities</h2>
+          <div className="flex flex-row justify-left">
+            <ul>
+              {amenities.map((amenity) => (
+                <li key={amenity}>
+                  <div className={`flex items-center space-x-3 rounded-lg px-2 py-1.5 dark:text-white `}>
+                    {<FontAwesomeIcon icon={amenitiesMap[amenity].icon} />}
+                    <span className="text-sm font-medium m-5">{amenity} &nbsp; </span>{amenitiesMap[amenity]?.description}
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <a
+          // if you are using Github OAuth, you can get rid of the Twitter option
+          href={
+            data.site?.user?.username
+              ? `https://twitter.com/${data.site.user.username}`
+              : `https://github.com/${data.site?.user?.gh_username}`
+          }
+          rel="noreferrer"
+          target="_blank"
+        >
+          <div className="my-8">
+            <div className="relative inline-block h-8 w-8 overflow-hidden rounded-full align-middle md:h-12 md:w-12">
+              {data.site?.user?.image ? (
+                <BlurImage
+                  alt={data.site?.user?.name ?? "User Avatar"}
+                  height={80}
+                  src={data.site.user.image}
+                  width={80}
+                />
+              ) : (
+                <div className="absolute flex h-full w-full select-none items-center justify-center bg-stone-100 text-4xl text-stone-500">
+                  ?
+                </div>
+              )}
+            </div>
+            <div className="text-md ml-3 inline-block align-middle dark:text-white md:text-lg">
+              by <span className="font-semibold">{data.site?.user?.name}</span>
+            </div>
+          </div>
+        </a>
+      </div >
+      <div className="relative m-auto mb-10 h-80 w-full max-w-screen-lg overflow-hidden md:mb-20 md:h-150 md:w-5/6 md:rounded-2xl lg:w-2/3">
+        <BlurImage
+          alt={data.title ?? "Property Image"}
+          width={1200}
+          height={630}
+          className="h-full w-full object-cover"
+          placeholder="blur"
+          blurDataURL={data.imageBlurhash ?? placeholderBlurhash}
+          src={data.image ?? "/placeholder.png"}
+        />
+      </div>
+
+      <MDX source={data.mdxSource} />
+
+      {
+        data.adjacentPosts.length > 0 && (
+          <div className="relative mb-20 mt-10 sm:mt-20">
+            <div
+              className="absolute inset-0 flex items-center"
+              aria-hidden="true"
+            >
+              <div className="w-full border-t border-stone-300 dark:border-stone-700" />
+            </div>
+            <div className="relative flex justify-center">
+              <span className="bg-white px-2 text-sm text-stone-500  dark:text-stone-400">
+                Other Rentals
+              </span>
+            </div>
+          </div>
+        )
+      }
+      {
+        data.adjacentPosts && (
+          <div className="mx-5 mb-20 grid max-w-screen-xl grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-2 xl:mx-auto xl:grid-cols-3">
+            {data.adjacentPosts.map((data, index) => (
+              <BlogCard key={index} data={data} />
+            ))}
+          </div>
+        )
+      }
+    </>
+  );
+}
+
+ChatGPT
+Based on the code you provided, I've made some fixes to resolve potential issues. Please find the corrected code below:
+
+jsx
+Copy code
+import React from "react";
+import { notFound } from "next/navigation";
+import { getPostData } from "@/lib/fetchers";
+import BlogCard from "@/components/blog-card";
+import BlurImage from "@/components/blur-image";
+import MDX from "@/components/mdx";
+import { placeholderBlurhash, toDateString } from "@/lib/utils";
+import ReservationForm from "@/components/booking/reservation-form";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faWifi,
+  faUtensils,
+  faTemperatureHigh,
+  faTv,
+  faTshirt,
+  faParking,
+  faSwimmingPool,
+  faDumbbell,
+  faHotTub,
+  faTree,
+  faUtensilSpoon,
+  faElevator,
+  faPaw,
+  faChild,
+  faSmoking,
+  faWheelchair,
+  faKey,
+  faClock,
+  faLaptop,
+  faCoffee,
+  faBicycle,
+  faFire,
+  faShieldAlt,
+  faConciergeBell,
+  // Add other icons here...
+} from "@fortawesome/free-solid-svg-icons";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { domain: string; slug: string };
+}) {
+  const { domain, slug } = params;
+  const data = await getPostData(domain, slug);
+  if (!data) {
+    return null;
+  }
+  const { id, title, description } = data;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      creator: "@ForHosts",
+    },
+  };
+}
+
+const amenitiesList: string[] = [
+  "Wifi",
+  "Kitchen Facilities",
+  "Heating and Air Conditioning",
+  "TV and Entertainment",
+  "Washer and Dryer",
+  "Free Parking",
+  "Pool",
+  "Gym/Fitness Center",
+  "Hot Tub/Jacuzzi",
+  "Outdoor Space",
+  "Barbecue/Grill",
+  "Elevator",
+  "Pets Allowed",
+  "Family/Kid-Friendly",
+  "Smoking Policy",
+  "Wheelchair Accessible",
+  "Self Check-in",
+  "24/7 Check-in",
+  "Workspace",
+  "Breakfast",
+  "Bikes",
+  "Fireplace",
+  "Security Features",
+  "Concierge Services",
+];
+
+const amenityDetails: { [key: string]: { icon: any; description: string } } = {
+  Wifi: {
+    icon: faWifi,
+    description: "Wifi available throughout the listing.",
+  },
+  "Kitchen Facilities": {
+    icon: faUtensils,
+    description: "Fully equipped kitchen with stove, oven, refrigerator, and cooking utensils.",
+  },
+  "Heating and Air Conditioning": {
+    icon: faTemperatureHigh,
+    description: "Climate control with heating and air conditioning.",
+  },
+  "TV and Entertainment": {
+    icon: faTv,
+    description: "Television with cable/satellite, streaming services, or DVD player.",
+  },
+  "Washer and Dryer": {
+    icon: faTshirt,
+    description: "Laundry facilities with washer and dryer.",
+  },
+  "Free Parking": {
+    icon: faParking,
+    description: "Free parking space available.",
+  },
+  Pool: {
+    icon: faSwimmingPool,
+    description: "Access to a swimming pool (private or shared).",
+  },
+  "Gym/Fitness Center": {
+    icon: faDumbbell,
+    description: "On-site gym or fitness center.",
+  },
+  "Hot Tub/Jacuzzi": {
+    icon: faHotTub,
+    description: "Private or shared hot tub for relaxation.",
+  },
+  "Outdoor Space": {
+    icon: faTree,
+    description: "Outdoor space, such as a patio, balcony, garden, or terrace.",
+  },
+  "Barbecue/Grill": {
+    icon: faUtensilSpoon,
+    description: "Barbecue or grill for outdoor cooking.",
+  },
+  Elevator: {
+    icon: faElevator,
+    description: "Elevator access (if applicable).",
+  },
+  "Pets Allowed": {
+    icon: faPaw,
+    description: "Pets are allowed in the listing.",
+  },
+  "Family/Kid-Friendly": {
+    icon: faChild,
+    description: "Amenities suitable for families with children.",
+  },
+  "Smoking Policy": {
+    icon: faSmoking,
+    description: "Smoking policy for the listing.",
+  },
+  "Wheelchair Accessible": {
+    icon: faWheelchair,
+    description: "Facilities for guests with mobility challenges.",
+  },
+  "Self Check-in": {
+    icon: faKey,
+    description: "Self check-in with keyless entry or lockbox.",
+  },
+  "24/7 Check-in": {
+    icon: faClock,
+    description: "Flexible check-in times, available 24/7.",
+  },
+  Workspace: {
+    icon: faLaptop,
+    description: "Designated workspace with a desk and chair.",
+  },
+  Breakfast: {
+    icon: faCoffee,
+    description: "Complimentary breakfast items or vouchers provided.",
+  },
+  Bikes: {
+    icon: faBicycle,
+    description: "Bicycles available for guest use.",
+  },
+  Fireplace: {
+    icon: faFire,
+    description: "Working fireplace (gas or wood-burning).",
+  },
+  "Security Features": {
+    icon: faShieldAlt,
+    description: "Security features like smoke detectors and first aid kits.",
+  },
+  "Concierge Services": {
+    icon: faConciergeBell,
+    description: "Concierge services for local recommendations and activities.",
+  },
+};
+
 const amenitiesMap: { [key: string]: { icon: any; description: string } } = amenitiesList.reduce(
-  (map, amenity: any) => {
-    type ObjectKey = keyof typeof amenityDetails;
-    const fieldName: any = amenity as ObjectKey;
-    map[fieldName] = amenityDetails[fieldName] || { icon: "default-icon", description: "" };
+  (map, amenity) => {
+    map[amenity] = amenityDetails[amenity] || { icon: "default-icon", description: "" };
     return map;
   },
   {}
@@ -224,3 +545,73 @@ export default async function SitePostPage({
     </>
   );
 }
+I've fixed the typings for amenityDetails, amenitiesList, and amenitiesMap objects to ensure they have the correct structure and data types. This should resolve the potential TypeScript errors. The rest of the JSX code remains unchanged, and it should work as expected.
+
+
+
+
+User
+from the code you gave me, i get this[{
+  "resource": "/c:/Users/ssyla/OneDrive/Desktop/ForHosts2/app/[domain]/[slug]/page.tsx",
+  "owner": "typescript",
+  "code": "7053",
+  "severity": 8,
+  "message": "Element implicitly has an 'any' type because expression of type 'string' can't be used to index type '{}'.\n  No index signature with a parameter of type 'string' was found on type '{}'.",
+  "source": "ts",
+  "startLineNumber": 194,
+  "startColumn": 5,
+  "endLineNumber": 194,
+  "endColumn": 17
+}]
+ChatGPT
+Apologies for the oversight.The error you are encountering is due to TypeScript not being able to infer the correct types for the amenitiesMap object.We can fix this by explicitly defining the type of amenitiesMap as shown below:
+
+jsx
+Copy code
+import React from "react";
+// ... (other imports)
+
+// Rest of your code remains unchanged
+
+const amenitiesMap: { [key: string]: { icon: any; description: string } } = amenitiesList.reduce(
+  (map, amenity) => {
+    map[amenity] = amenityDetails[amenity] || { icon: "default-icon", description: "" };
+    return map;
+  },
+  {}
+);
+
+// Rest of your code remains unchanged
+By providing explicit type annotations for amenitiesMap, TypeScript should be able to correctly infer the types and resolve the error.This should fix the TypeScript error you were encountering.The rest of your code should work as expected.
+
+
+
+
+  User
+that is the same code
+ChatGPT
+Apologies for the confusion.You are right; that code was the same.Let's update the amenitiesMap definition to fix the TypeScript error:
+
+jsx
+Copy code
+import React from "react";
+// ... (other imports)
+
+// Rest of your code remains unchanged
+
+const amenitiesMap: { [key: string]: { icon: any; description: string } } = amenitiesList.reduce(
+  (map, amenity) => {
+    map[amenity] = amenityDetails[amenity as keyof typeof amenityDetails] || { icon: "default-icon", description: "" };
+    return map;
+  },
+  {}
+);
+
+// Rest of your code remains unchanged
+By adding as keyof typeof amenityDetails after amenity when accessing amenityDetails, we explicitly inform TypeScript that amenity is a valid key of the amenityDetails object.This should resolve the TypeScript error, and the code should work as expected without any issues.
+
+
+
+
+
+
