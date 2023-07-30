19e94e2747df04c7cd9795416e2ee39145fc9767
KWSaunders <kwsaunders@pipeline.sbcc.edu>
2023-07-29 18:22:32 -0700

    stripe integration

--- app/[domain]/[slug]/checkout/page.tsx
+++ app/[domain]/[slug]/checkout/page.tsx
@@ -1,4 +1,14 @@
+import DonatePage from "@/components/stripe/donate-with-elements";
+
 export default async function CheckoutPage() {
 
+    // set the price
+
+    return (
+        <div>
+            <h1>Kekkers</h1>
+            <DonatePage />
+        </div>
+    );
 
 }
--- components/posts.tsx
+++ components/posts.tsx
@@ -15,39 +15,39 @@ export default async function Posts({
   if (!session?.user) {
     redirect("/login");
   }
-  // const posts = await prisma.post.findMany({
-  //   where: {
-  //     userId: session.user.id as string,
-  //     ...(siteId ? { siteId } : {}),
-  //   },
-  //   orderBy: {
-  //     updatedAt: "desc",
-  //   },
-  //   include: {
-  //     site: true,
-  //   },
-  //   ...(limit ? { take: limit } : {}),
-  // });
+  const posts = await prisma.post.findMany({
+    where: {
+      userId: session.user.id as string,
+      ...(siteId ? { siteId } : {}),
+    },
+    orderBy: {
+      updatedAt: "desc",
+    },
+    include: {
+      site: true,
+    },
+    ...(limit ? { take: limit } : {}),
+  });
 
-  // return posts.length > 0 ? (
-  //   <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-4">
-  //     {posts.map((post) => (
-  //       <PostCard key={post.id} data={post} />
-  //     ))}
-  //   </div>
-  // ) : (
-  //   <div className="flex flex-col items-center space-x-4">
-  //     <h1 className="font-cal text-4xl">No Properties Yet</h1>
-  //     <Image
-  //       alt="missing post"
-  //       src="https://illustrations.popsy.co/gray/graphic-design.svg"
-  //       width={400}
-  //       height={400}
-  //     />
-  //     <p className="text-lg text-stone-500">
-  //       You do not have any properties yet. Create one to get started.
-  //     </p>
-  //   </div>
-  // );
+  return posts.length > 0 ? (
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-4">
+      {posts.map((post) => (
+        <PostCard key={post.id} data={post} />
+      ))}
+    </div>
+  ) : (
+    <div className="flex flex-col items-center space-x-4">
+      <h1 className="font-cal text-4xl">No Properties Yet</h1>
+      <Image
+        alt="missing post"
+        src="https://illustrations.popsy.co/gray/graphic-design.svg"
+        width={400}
+        height={400}
+      />
+      <p className="text-lg text-stone-500">
+        You do not have any properties yet. Create one to get started.
+      </p>
+    </div>
+  );
   return (<></>);
 }
