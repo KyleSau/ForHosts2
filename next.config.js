986ca345c6fe28e9f27ddc054c79b0c0fb810bcc
KWSaunders <kwsaunders@pipeline.sbcc.edu>
2023-07-29 19:38:16 -0700

    dropped db

--- next.config.js
+++ next.config.js
@@ -18,7 +18,8 @@ module.exports = {
       "lh3.googleusercontent.com",
       "www.facebook.com",
       "platform-lookaside.fbsbx.com",
-      "ccd1kw3boji7y6xm.public.blob.vercel-storage.com"
+      "ccd1kw3boji7y6xm.public.blob.vercel-storage.com",
+      "stripe.com"
     ],
   },
   reactStrictMode: false,
