e7e68a84a9c8284737548f29481bd5546c9195bf
Kyle Saunders <kwsaunders@pipeline.sbcc.edu>
2023-07-31 02:49:55 -0700

    dsfadF

--- app/api/checkout_sessions/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import handler from '../checkout_sessions'
-
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-    return handler(req, res)
-}
