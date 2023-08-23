884e8b94a9c9573de1e560d8e27b04f8338ec00b
mcdoodle97 <mseamons5@hotmail.com>
2023-08-23 15:10:47 -0700

    fixes to types

--- components/dash-site-page/image-gallery.tsx
+++ components/dash-site-page/image-gallery.tsx
@@ -21,9 +21,9 @@ interface ImageGalleryProps {
             src={image ?? placeholderImage}
           />
         </div>
-        <div className="relative grid grid-cols-2 gap-3 ml-4 w-2/3 h-full">
-          {Array(4).fill().map((_, index) => (
-            <div key={index} className="relative h-full w-full overflow-hidden rounded-2xl hover:scale-105">
+        <div className="relative grid grid-cols-2 gap-2 ml-4 w-2/3 h-full">
+        {Array(4).fill(null).map((_, index) => (
+            <div key={index} className="relative h-full w-full overflow-hidden rounded-2xl hover:shadow-lg hover:border-2 border-gray-600 hover:opacity-90">
               <BlurImage
                 alt={`Additional Property Image ${index + 1}`}
                 width={300}
