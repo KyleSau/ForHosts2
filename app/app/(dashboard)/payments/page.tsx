5cfd923ddee0c1b61dc4dd5917faf3bba18b11c6
Kyle Saunders <kwsaunders@pipeline.sbcc.edu>
2023-08-27 13:10:21 -0700

    removed unused imports

--- app/app/(dashboard)/payments/page.tsx
+++ app/app/(dashboard)/payments/page.tsx
@@ -1,16 +1,5 @@
-import { getSession } from "@/lib/auth";
-import HomeView from '@/components/stripe/HomeView';
-import { fetchStripeData } from "@/lib/actions";
-import { NextPageContext } from "next";
-import { ParsedUrlQuery } from 'querystring';
-import { GetServerSideProps } from "next";
 
-type Props = {
-    params: {},
-    searchParams: { [key: string]: string | string[] | undefined },
-}
 
-// export default async function PaymentsPage(props: Props) {
 export default function Page({ }) {
 
     return <h1>Test</h1>
