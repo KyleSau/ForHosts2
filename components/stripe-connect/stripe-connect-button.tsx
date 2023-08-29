af09e65875c7d83203fd79a929a2ff03f536b9f7
Kyle Saunders <kwsaunders@pipeline.sbcc.edu>
2023-08-28 18:43:32 -0700

    env

--- components/stripe-connect/stripe-connect-button.tsx
+++ components/stripe-connect/stripe-connect-button.tsx
@@ -5,7 +5,7 @@ interface Props {
 const StripeConnectButton: React.FC<Props> = ({ }) => {
 
     const handleClick = () => {
-        const url = `https://dashboard.stripe.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_STRIPE_OAUTH_CLIENT_ID}&scope=read_write&state=${Math.random() * 100}&redirect_uri=${process.env.NEXT_PUBLIC_BASE_URL}`;
+        const url = `https://dashboard.stripe.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_STRIPE_OAUTH_CLIENT_ID}&scope=read_write&state=${Math.random() * 100}&redirect_uri=${process.env.NEXT_PUBLIC_PAYMENTS_URL}`;
         window.document.location.href = url;
     };
 
