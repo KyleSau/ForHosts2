3bbc13e6306ad453277251b67158e0625ae48397
mcdoodle97 <mseamons5@hotmail.com>
2023-09-02 16:15:16 -0700

    new editor changes

--- components/editor/index.tsx
+++ components/editor/index.tsx
@@ -31,6 +31,12 @@ export default function Editor({ post }: { post: PostWithSite }) {
   const [showCheckTimes, setShowCheckTimes] = useState(true);
   const [showPropertyDetails, setShowPropertyDetails] = useState(false);
   const [showAddPhotoArea, setShowAddPhotoArea] = useState(true);
+ 
+  const [activeTab, setActiveTab] = useState('app'); // Initialize the active tab state
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab); // Set the active tab when a button is clicked
+  }
 
   const sliderIntervals = ["1 mo", "2 mo", "3 mo", "4 mo", "5 mo", "6 mo", "1 yr", "custom"];
   const [sliderIdx, setSliderIdx] = useState(0);
@@ -223,10 +229,82 @@ export default function Editor({ post }: { post: PostWithSite }) {
     }
     setData({ ...data, availabilityWindow });
   };
-
+ 
   return (
+    
+      
     <div className="relative min-h-[500px] w-full max-w-screen-lg border-stone-200 p-12 px-8 dark:border-stone-700 sm:mb-[calc(20vh)] sm:rounded-lg sm:border sm:px-12 sm:shadow-lg">
-      <div className="absolute right-5 top-5 mb-5 flex items-center space-x-3">
+      <div className="relative">
+      <ul className="relative flex flex-wrap p-1 list-none bg-transparent rounded-xl" nav-pills role="tablist">
+      <li className={` flex-auto text-center ${activeTab === 'app' ? 'bg-gray-200 rounded-xl text-gray-600 shadow-md' : 'bg-transparent rounded-xl text-slate-700 w-'}`}>
+            <a
+              className="flex flex-col items-center w-full px-0 py-1 mb-0 transition-all border-0 rounded-lg ease-soft-in-out bg-inherit text-slate-700"
+              href="javascript:;"
+              onClick={() => handleTabClick('app')} // Set the active tab when clicking
+              aria-selected={activeTab === 'app' ? 'true' : 'false'}
+            >
+              <svg className="text-slate-700" width="16px" height="16px" viewBox="0 0 42 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+          <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+            <g transform="translate(-2319.000000, -291.000000)" fill="#FFFFFF" fillRule="nonzero">
+              <g transform="translate(1716.000000, 291.000000)">
+                <g transform="translate(603.000000, 0.000000)">
+                  <path className="fill-slate-800" d="M22.7597136,19.3090182 L38.8987031,11.2395234 C39.3926816,10.9925342 39.592906,10.3918611 39.3459167,9.89788265 C39.249157,9.70436312 39.0922432,9.5474453 38.8987261,9.45068056 L20.2741875,0.1378125 L20.2741875,0.1378125 C19.905375,-0.04725 19.469625,-0.04725 19.0995,0.1378125 L3.1011696,8.13815822 C2.60720568,8.38517662 2.40701679,8.98586148 2.6540352,9.4798254 C2.75080129,9.67332903 2.90771305,9.83023153 3.10122239,9.9269862 L21.8652864,19.3090182 C22.1468139,19.4497819 22.4781861,19.4497819 22.7597136,19.3090182 Z"></path>
+                  <path className="fill-slate-800" d="M23.625,22.429159 L23.625,39.8805372 C23.625,40.4328219 24.0727153,40.8805372 24.625,40.8805372 C24.7802551,40.8805372 24.9333778,40.8443874 25.0722402,40.7749511 L41.2741875,32.673375 L41.2741875,32.673375 C41.719125,32.4515625 42,31.9974375 42,31.5 L42,14.241659 C42,13.6893742 41.5522847,13.241659 41,13.241659 C40.8447549,13.241659 40.6916418,13.2778041 40.5527864,13.3472318 L24.1777864,21.5347318 C23.8390024,21.7041238 23.625,22.0503869 23.625,22.429159 Z" opacity="0.7"></path>
+                  <path className="fill-slate-800" d="M20.4472136,21.5347318 L1.4472136,12.0347318 C0.953235098,11.7877425 0.352562058,11.9879669 0.105572809,12.4819454 C0.0361450918,12.6208008 6.47121774e-16,12.7739139 0,12.929159 L0,30.1875 L0,30.1875 C0,30.6849375 0.280875,31.1390625 0.7258125,31.3621875 L19.5528096,40.7750766 C20.0467945,41.0220531 20.6474623,40.8218132 20.8944388,40.3278283 C20.963859,40.1889789 21,40.0358742 21,39.8806379 L21,22.429159 C21,22.0503869 20.7859976,21.7041238 20.4472136,21.5347318 Z" opacity="0.7"></path>
+                </g>
+              </g>
+            </g>
+          </g>
+        </svg>
+        <span className="ml-1">General Info</span>
+      </a>
+    </li>
+    <li className={`z-30 flex-auto text-center ${activeTab === 'HomeInfo' ? 'bg-gray-200 rounded-xl text-gray-600 shadow-md' : 'bg-transparent rounded-xl text-slate-700 w-'}`}>
+    <a
+              className="flex flex-col items-center w-full px-0 py-1 mb-0 transition-all border-0 rounded-lg ease-soft-in-out bg-inherit text-slate-700"
+              href="javascript:;"
+              onClick={() => handleTabClick('HomeInfo')} // Set the active tab when clicking
+            >
+      <svg className="text-slate-700" width="16px" height="16px" viewBox="0 0 40 44" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+          <title>document</title>
+          <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+            <g transform="translate(-1870.000000, -591.000000)" fill="#FFFFFF" fillRule="nonzero">
+              <g transform="translate(1716.000000, 291.000000)">
+                <g transform="translate(154.000000, 300.000000)">
+                  <path className="fill-slate-800" d="M40,40 L36.3636364,40 L36.3636364,3.63636364 L5.45454545,3.63636364 L5.45454545,0 L38.1818182,0 C39.1854545,0 40,0.814545455 40,1.81818182 L40,40 Z" opacity="0.603585379"></path>
+                  <path className="fill-slate-800" d="M30.9090909,7.27272727 L1.81818182,7.27272727 C0.814545455,7.27272727 0,8.08727273 0,9.09090909 L0,41.8181818 C0,42.8218182 0.814545455,43.6363636 1.81818182,43.6363636 L30.9090909,43.6363636 C31.9127273,43.6363636 32.7272727,42.8218182 32.7272727,41.8181818 L32.7272727,9.09090909 C32.7272727,8.08727273 31.9127273,7.27272727 30.9090909,7.27272727 Z M18.1818182,34.5454545 L7.27272727,34.5454545 L7.27272727,30.9090909 L18.1818182,30.9090909 L18.1818182,34.5454545 Z M25.4545455,27.2727273 L7.27272727,27.2727273 L7.27272727,23.6363636 L25.4545455,23.6363636 L25.4545455,27.2727273 Z M25.4545455,20 L7.27272727,20 L7.27272727,16.3636364 L25.4545455,16.3636364 L25.4545455,20 Z"></path>
+                </g>
+              </g>
+            </g>
+          </g>
+        </svg>
+        <span className="ml-1">Home Information</span>
+      </a>
+    </li>
+    <li className="flex-auto text-center">
+    <a className=" flex flex-col items-center w-full px-0 py-1 mb-0 transition-all border-0 rounded-lg ease-soft-in-out bg-inherit text-slate-700" nav-link href="javascript:;" role="tab" aria-selected="false">
+        <svg className="text-slate-700" width="16px" height="16px" viewBox="0 0 40 40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+          <title>settings</title>
+          <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+            <g transform="translate(-2020.000000, -442.000000)" fill="#FFFFFF" fillRule="nonzero">
+              <g transform="translate(1716.000000, 291.000000)">
+                <g transform="translate(304.000000, 151.000000)">
+                  <polygon className="fill-slate-800" opacity="0.596981957" points="18.0883333 15.7316667 11.1783333 8.82166667 13.3333333 6.66666667 6.66666667 0 0 6.66666667 6.66666667 13.3333333 8.82166667 11.1783333 15.315 17.6716667"></polygon>
+                  <path className="fill-slate-800" d="M31.5666667,23.2333333 C31.0516667,23.2933333 30.53,23.3333333 30,23.3333333 C29.4916667,23.3333333 28.9866667,23.3033333 28.48,23.245 L22.4116667,30.7433333 L29.9416667,38.2733333 C32.2433333,40.575 35.9733333,40.575 38.275,38.2733333 L38.275,38.2733333 C40.5766667,35.9716667 40.5766667,32.2416667 38.275,29.94 L31.5666667,23.2333333 Z" opacity="0.596981957"></path>
+                  <path className="fill-slate-800" d="M33.785,11.285 L28.715,6.215 L34.0616667,0.868333333 C32.82,0.315 31.4483333,0 30,0 C24.4766667,0 20,4.47666667 20,10 C20,10.99 20.1483333,11.9433333 20.4166667,12.8466667 L2.435,27.3966667 C0.95,28.7083333 0.0633333333,30.595 0.00333333333,32.5733333 C-0.0583333333,34.5533333 0.71,36.4916667 2.11,37.89 C3.47,39.2516667 5.27833333,40 7.20166667,40 C9.26666667,40 11.2366667,39.1133333 12.6033333,37.565 L27.1533333,19.5833333 C28.0566667,19.8516667 29.01,20 30,20 C35.5233333,20 40,15.5233333 40,10 C40,8.55166667 39.685,7.18 39.1316667,5.93666667 L33.785,11.285 Z"></path>
+                </g>
+              </g>
+            </g>
+          </g>
+        </svg>
+        <span className="ml-1">Listing Settings</span>
+      </a>
+    </li>
+  </ul>
+      </div>
+      <hr />
+      {/* <div className="absolute right-5 top-5 mb-5 flex items-center space-x-3">
+        
         {data.published && (
           <a
             href={url}
@@ -271,27 +349,111 @@ export default function Editor({ post }: { post: PostWithSite }) {
             <p>{data.published ? "Edit Listing" : "Publish Listing"}</p>
           )}
         </button>
-      </div>
-      <div className="mb-5 flex flex-col space-y-3 border-b border-stone-200 pb-5 dark:border-stone-700">
-        <input
-          type="text"
-          placeholder="Title of Property"
-          defaultValue={post?.title || ""}
-          autoFocus
-          onChange={(e) => setData({ ...data, title: e.target.value })}
-          className="dark:placeholder-text-600 border-none px-0 font-cal text-3xl placeholder:text-stone-400 focus:outline-none  focus:ring-0"
-        />
-
-        <TextareaAutosize
-          placeholder="Description of Property"
-          defaultValue={post?.description || ""}
-          onChange={(e) => setData({ ...data, description: e.target.value })}
-          className="dark:placeholder-text-600 w-full resize-none border-none px-0 placeholder:text-stone-400 focus:outline-none  focus:ring-0"
-        />
-      </div>
-      {editor && <EditorBubbleMenu editor={editor} />}
-      <EditorContent editor={editor} />
-      <div
+      </div> */}
+          {activeTab === 'app' && (
+            <div className="p-5">
+    <div className="mx-4 p-4">
+        <div className="flex items-center">
+            <div className="flex items-center text-teal-600 relative">
+                <div className="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 border-teal-600">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-bookmark ">
+                        <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+                    </svg>
+                </div>
+                <div className="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-teal-600">Personal</div>
+            </div>
+            <div className="flex-auto border-t-2 transition duration-500 ease-in-out border-teal-600"></div>
+            <div className="flex items-center text-white relative">
+                <div className="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 bg-teal-600 border-teal-600">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-user-plus ">
+                        <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                        <circle cx="8.5" cy="7" r="4"></circle>
+                        <line x1="20" y1="8" x2="20" y2="14"></line>
+                        <line x1="23" y1="11" x2="17" y2="11"></line>
+                    </svg>
+                </div>
+                <div className="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-teal-600">Account</div>
+            </div>
+            <div className="flex-auto border-t-2 transition duration-500 ease-in-out border-gray-300"></div>
+            <div className="flex items-center text-gray-500 relative">
+                <div className="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 border-gray-300">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-mail ">
+                        <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+                        <polyline points="22,6 12,13 2,6"></polyline>
+                    </svg>
+                </div>
+                <div className="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-gray-500">Message</div>
+            </div>
+            <div className="flex-auto border-t-2 transition duration-500 ease-in-out border-gray-300"></div>
+            <div className="flex items-center text-gray-500 relative">
+                <div className="rounded-full transition duration-500 ease-in-out h-12 w-12 py-3 border-2 border-gray-300">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-database ">
+                        <ellipse cx="12" cy="5" rx="9" ry="3"></ellipse>
+                        <path d="M21 12c0 1.66-4 3-9 3s-9-1.34-9-3"></path>
+                        <path d="M3 5v14c0 1.66 4 3 9 3s9-1.34 9-3V5"></path>
+                    </svg>
+                </div>
+                <div className="absolute top-0 -ml-10 text-center mt-16 w-32 text-xs font-medium uppercase text-gray-500">Confirm</div>
+            </div>
+        </div>
+    </div>
+    <div className="mt-8 p-4">
+        <div>
+            <div className="font-bold text-gray-600 text-xs leading-8 uppercase h-6 mx-2 mt-3">Full Name</div>
+            <div className="flex flex-col md:flex-row">
+                <div className="w-full flex-1 mx-2 svelte-1l8159u">
+                    <div className="bg-white my-2 p-1 flex border border-gray-200 rounded svelte-1l8159u">
+                        <input placeholder="First Name" className="p-1 px-2 appearance-none outline-none w-full text-gray-800" /> </div>
+                </div>
+                <div className="w-full flex-1 mx-2 svelte-1l8159u">
+                    <div className="bg-white my-2 p-1 flex border border-gray-200 rounded svelte-1l8159u">
+                        <input placeholder="Last Name" className="p-1 px-2 appearance-none outline-none w-full text-gray-800"/> </div>
+                </div>
+            </div>
+            <div className="flex flex-col md:flex-row">
+                <div className="w-full mx-2 flex-1 svelte-1l8159u">
+                    <div className="font-bold h-6 mt-3 text-gray-600 text-xs leading-8 uppercase"> Username</div>
+                    <div className="bg-white my-2 p-1 flex border border-gray-200 rounded svelte-1l8159u">
+                        <input placeholder="Just a hint.." className="p-1 px-2 appearance-none outline-none w-full text-gray-800"/> </div>
+                </div>
+                <div className="w-full mx-2 flex-1 svelte-1l8159u">
+                    <div className="font-bold h-6 mt-3 text-gray-600 text-xs leading-8 uppercase"> Your Email</div>
+                    <div className="bg-white my-2 p-1 flex border border-gray-200 rounded svelte-1l8159u">
+                        <input placeholder="jhon@doe.com" className="p-1 px-2 appearance-none outline-none w-full text-gray-800"/> </div>
+                </div>
+            </div>
+        </div>
+        <div className="flex p-2 mt-4">
+            <button className="text-base hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer 
+        hover:bg-gray-200  
+        bg-gray-100 
+        text-gray-700 
+        border duration-200 ease-in-out 
+        border-gray-600 transition">Previous</button>
+            <div className="flex-auto flex flex-row-reverse">
+                <button className="text-base  ml-2  hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer 
+        hover:bg-teal-600  
+        bg-teal-600 
+        text-teal-100 
+        border duration-200 ease-in-out 
+        border-teal-600 transition">Next</button>
+                <button className="text-base hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer 
+        hover:bg-teal-200  
+        bg-teal-100 
+        text-teal-700 
+        border duration-200 ease-in-out 
+        border-teal-600 transition">Skip</button>
+            </div>
+        </div>
+    </div>
+</div>
+      )}
+      {activeTab === 'HomeInfo' && (
+        <div>test</div>
+      )}
+      {/* {editor && <EditorBubbleMenu editor={editor} />} */}
+      {/* <EditorContent editor={editor} /> */}
+      {/* <div
         className="flex cursor-pointer justify-center"
         onClick={() => setShowCheckTimes(!showCheckTimes)}
       >
@@ -306,6 +468,7 @@ export default function Editor({ post }: { post: PostWithSite }) {
           transition: "max-height 0.5s ease-in-out",
         }}
       >
+      
         <div className="mb-2">
           <h2 className="font-cal text-xl font-bold">In-Service Date</h2>
           <input
@@ -418,7 +581,7 @@ export default function Editor({ post }: { post: PostWithSite }) {
             className="dark:placeholder-text-600 placeholder-text-stone-400 w-full rounded-md border border-black px-0 font-cal text-xl focus:border-black focus:bg-sitecolor focus:outline-none focus:ring-0"
           />
         </div>
-
+  
         <div className="mb-4">
           <input 
             type="number"
@@ -497,7 +660,7 @@ export default function Editor({ post }: { post: PostWithSite }) {
             className="dark:placeholder-text-600 placeholder-text-stone-400 w-full rounded-md border border-black px-0 font-cal text-xl focus:border-black focus:bg-sitecolor focus:outline-none focus:ring-0"
           />
         </div>
-
+ 
         <input
           type="number"
           placeholder="Security deposit"
@@ -528,7 +691,10 @@ export default function Editor({ post }: { post: PostWithSite }) {
           componentId="listing-photos-drag-drop-area"
           
         />
-      </div>
+      
+      </div> */}
+
     </div>
+    
   );
 }
