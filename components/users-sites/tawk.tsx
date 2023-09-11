12a70be5ea014d7d900afcf57cae3c16695fa9b2
ssssylassss <125253203+ssssylassss@users.noreply.github.com>
2023-09-11 18:01:26 -0500

    comment out tawk for now

--- components/users-sites/tawk.tsx
+++ components/users-sites/tawk.tsx
@@ -1,26 +1,26 @@
-import "../styles/globals.css";
-import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
-import { useRef } from 'react';
+// import "../styles/globals.css";
+// import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
+// import { useRef } from 'react';
 
-function MyApp({ Component, pageProps }: { Component: React.ComponentType<any>; pageProps: any }) {
-    const tawkMessengerRef = useRef();
+// function MyApp({ Component, pageProps }: { Component: React.ComponentType<any>; pageProps: any }) {
+//     const tawkMessengerRef = useRef();
 
-    const handleMinimize = () => {
-        if (tawkMessengerRef.current)
-            (tawkMessengerRef.current as any).minimize();
-    };
-    return (
-        <>
-            <Component {...pageProps} />
-            <button onClick={handleMinimize}> Minimize the Chat </button>
+//     const handleMinimize = () => {
+//         if (tawkMessengerRef.current)
+//             (tawkMessengerRef.current as any).minimize();
+//     };
+//     return (
+//         <>
+//             <Component {...pageProps} />
+//             <button onClick={handleMinimize}> Minimize the Chat </button>
 
-            <TawkMessengerReact
-                propertyId={process.env.NEXT_PUBLIC_TWAKTO_PROPERTY_ID}
-                widgetId={process.env.NEXT_PUBLIC_TWAKTO_WIDGET_ID}
-                useRef={tawkMessengerRef}
-            />
-        </>
-    );
-}
+//             <TawkMessengerReact
+//                 propertyId={process.env.NEXT_PUBLIC_TWAKTO_PROPERTY_ID}
+//                 widgetId={process.env.NEXT_PUBLIC_TWAKTO_WIDGET_ID}
+//                 useRef={tawkMessengerRef}
+//             />
+//         </>
+//     );
+// }
 
-export default MyApp;
+// export default MyApp;
