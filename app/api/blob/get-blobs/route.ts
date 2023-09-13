8275e505639f20ab33888fc82910076520f9027e
mcxu <michaelxu79@gmail.com>
2023-09-12 21:39:36 -0400

    fixing edge function declaration

--- app/api/blob/get-blobs/route.ts
+++ app/api/blob/get-blobs/route.ts
@@ -1,10 +1,8 @@
 import { list } from '@vercel/blob';
 import { NextResponse } from 'next/server';
 
-export const config = {
-  runtime: 'edge',
-};
- 
+export const runtime = 'edge';
+
 export async function GET(request: Request) {
   const { blobs } = await list();
   // console.log("type of blobs: ", typeof(blobs));
