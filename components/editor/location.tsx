420e34aa69243ffec6ca97c90347f201c048c6b2
mcdoodle97 <mseamons5@hotmail.com>
2023-09-26 21:46:44 -0700

    google autocomplete address and geocoding to extract lat n lng information

--- components/editor/location.tsx
+++ components/editor/location.tsx
@@ -6,6 +6,10 @@ import TabTitle from "./tab-title";
 import { useTransition } from "react";
 import EditorSaveButton from "./editor-save-button";
 import EditorWrapper from "./editor-container-wrapper";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from "react-places-autocomplete";
 export default function Location() {
   const [submitted, setSubmitted] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
@@ -13,11 +17,11 @@ export default function Location() {
   let [isPendingPublishing, startTransitionPublishing] = useTransition();
 
   const validationSchema = Yup.object().shape({
-    title: Yup.string().required("Title is required"),
-    description: Yup.string().required("Description is required"),
+    // title: Yup.string().required("Title is required"),
+    // description: Yup.string().required("Description is required"),
     // bedrooms: Yup.number().required('Number of bedrooms is required').min(1, 'Must be at least 1'),
     // bathrooms: Yup.number().required('Number of bathrooms is required').min(1, 'Must be at least 1'),
-    // location: Yup.string().required('Street Address is required'),
+    location: Yup.string().required("Street Address is required"),
     // streetAddress: Yup.string().required('Street address is required'),
     // city: Yup.string().required('City is required'),
     // region: Yup.string().required('State / Province is required'),
@@ -31,27 +35,31 @@ export default function Location() {
       siteId: "",
 
       location: "",
-      country: "",
-      streetAddress: "",
-      city: "",
-      region: "",
-      postalCode: "",
+      // country: "",
+      // streetAddress: "",
+      // city: "",
+      // region: "",
+      // postalCode: "",
     },
     validationSchema: validationSchema,
     onSubmit: async (values) => {
       setSubmitted(false);
-      setIsLoading(true);
+      try {
+        const results = await geocodeByAddress(values.location);
+        const latLng = await getLatLng(results[0]);
 
-      console.log(formData);
-      //  form submission logic
-      //   const result = await updatePost(values);
+        console.log("Latitude:", latLng.lat);
+        console.log("Longitude:", latLng.lng);
 
-      if (result?.error) {
-        console.error(result.error);
-        setSubmitted(false);
-      } else {
-        console.log("Post updated successfully:", result);
+        // Continue with form submission logic
+        // For example, you can save the latLng values to your backend here
+        // and then set submitted and isLoading states accordingly
+
         setSubmitted(true);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error geocoding address:", error);
+        setSubmitted(false);
         setIsLoading(false);
       }
     },
@@ -72,43 +80,68 @@ export default function Location() {
   }, [formik.dirty]);
   return (
     <EditorWrapper>
-      <div>
-        <TabTitle
-          title="Address Information"
-          desc="Address Information regarding your listing (This will not be shown to anyone until 24 hours before check-in)"
-        />
-        <div className="mt-6 grid grid-cols-2 gap-4">
-          <div className="space-y-4">
-            <label
-              htmlFor="location"
-              className="block text-sm font-medium leading-6  text-gray-900"
-            >
-              Street Address
-            </label>
-            <input
-              type="text"
-              name="location"
-              id="location"
-              className={` w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${formik.touched.location && formik.errors.location
-                ? "border-red-500"
-                : ""
-                }`}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.location}
-            />
-            {formik.touched.title && formik.errors.title && (
-              <div className="mt-2 text-sm text-red-600">
-                {formik.errors.location}
+      <form onSubmit={formik.handleSubmit}>
+        <div className="space-y-4">
+          <label
+            htmlFor="location"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Street Address
+          </label>
+          <PlacesAutocomplete
+            value={formik.values.location}
+            onChange={(address) => formik.setFieldValue("location", address)}
+            onSelect={(address) => formik.setFieldValue("location", address)}
+          >
+            {({
+              getInputProps,
+              suggestions,
+              getSuggestionItemProps,
+              loading,
+            }) => (
+              <div>
+                <input
+                  {...getInputProps({
+                    placeholder: "Search for an address...",
+                    className: `w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 ${
+                      formik.touched.location && formik.errors.location
+                        ? "border-red-500"
+                        : ""
+                    }`,
+                  })}
+                />
+                <div>
+                  {loading && <div>Loading...</div>}
+                  {suggestions.map((suggestion) => {
+                    const className = suggestion.active
+                      ? "suggestion-item--active"
+                      : "suggestion-item";
+                    return (
+                      <div
+                        {...getSuggestionItemProps(suggestion, {
+                          className,
+                        })}
+                      >
+                        {suggestion.description}
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
             )}
-          </div>
+          </PlacesAutocomplete>
+          {formik.touched.location && formik.errors.location && (
+            <div className="mt-2 text-sm text-red-600">
+              {formik.errors.location}
+            </div>
+          )}
+          <EditorSaveButton
+            dirty={formik.dirty}
+            submitted={submitted}
+            isLoading={isLoading}
+          />
         </div>
-        <hr className="mt-8" />
-        <div className="mt-4">
-          <EditorSaveButton dirty={formik.dirty} submitted={submitted} isLoading={isLoading} />
-        </div>
-      </div>
+      </form>
     </EditorWrapper>
   );
 }
