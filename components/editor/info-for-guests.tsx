8f8721efc3008a40aa2d06018b4ae6698ca44a74
ssssylassss <125253203+ssssylassss@users.noreply.github.com>
2023-10-04 03:23:28 -0500

    info

--- components/editor/info-for-guests.tsx
+++ components/editor/info-for-guests.tsx
@@ -6,12 +6,9 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '.
 import { Label } from '../ui/label';
 import { Input } from '../ui/input';
 import EditorWrapper from './editor-container-wrapper';
-// import {
-//     Tooltip,
-//     TooltipContent,
-//     TooltipProvider,
-//     TooltipTrigger,
-// } from "@/components/ui/tooltip"
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../ui/tooltip';
+import WYSIWYGEditor from './wysiwyg-editor';
+
 
 
 export default function InfoForGuests({ data }) {
@@ -73,16 +70,15 @@ export default function InfoForGuests({ data }) {
                                     ></SelectValue>
                                 </SelectTrigger>
                                 <SelectContent className="max-w-[205px] mt-2 border rounded shadow-lg h-[200px]">
-  {hours.map((hour) => (
-    <SelectItem
-      value={hour} // Use the current hour as the value
-      key={hour}
-    >
-      {hour}
-    </SelectItem>
-  ))}
-</SelectContent>
-
+                                    {hours.map((hour) => (
+                                        <SelectItem
+                                            value={hour} // Use the current hour as the value
+                                            key={hour}
+                                        >
+                                            {hour}
+                                        </SelectItem>
+                                    ))}
+                                </SelectContent>
                             </Select>
                             <Select>
                                 <SelectTrigger className="max-w-[205px] justify-center border p-2 rounded">
@@ -93,14 +89,14 @@ export default function InfoForGuests({ data }) {
                                 </SelectTrigger>
                                 <SelectContent className="max-w-[205px] mt-2 border rounded shadow-lg h-[200px]">
                                     {hours.map((hour) => (
-                         <SelectItem
-                         value={hour} // Use the current hour as the value
-                         key={hour}
-                       >
-                         {hour}
-                       </SelectItem>
-                     ))}
-                   </SelectContent>
+                                        <SelectItem
+                                            value={hour} // Use the current hour as the value
+                                            key={hour}
+                                        >
+                                            {hour}
+                                        </SelectItem>
+                                    ))}
+                                </SelectContent>
                             </Select>
                         </div>
                     </div>
@@ -113,7 +109,7 @@ export default function InfoForGuests({ data }) {
                                     <SelectValue
                                         placeholder={formik.values.checkoutTime}
                                         onChange={formik.handleChange}
-                                   
+
                                     ></SelectValue>
                                 </SelectTrigger>
                                 <SelectContent className="max-w-[133px] mt-2 border rounded h-[200px]">
