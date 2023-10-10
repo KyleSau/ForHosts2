d1bb62759053d4744eae5efcb4c23719bce1d7b7
mcdoodle97 <mseamons5@hotmail.com>
2023-10-10 09:11:10 -0700

    site page changes

--- app/[domain]/[slug]/page.tsx
+++ app/[domain]/[slug]/page.tsx
@@ -12,7 +12,7 @@ import { CalendarDemo } from "@/components/ui/uicalendar";
 import ShowMoreModal from "@/components/users-sites/show-more-modal"
 import ImageGallery from "@/components/dash-site-page/image-gallery";
 import DashHeader from "@/components/dash-site-page/dash-header";
-import { useMap, Circle } from "react-leaflet";
+// import { useMap, Circle } from "react-leaflet";
 // import OpenStreetMap from '../component/OpenStreetMap'
 
 export async function generateMetadata({
@@ -53,7 +53,7 @@ export default async function SitePostPage({
   if (!data) {
     notFound();
   }
-  const map = useMap();
+ 
   const post = await prisma.post.findUnique({
     where: {
       id: data.id,
@@ -75,24 +75,27 @@ export default async function SitePostPage({
   return (
     <>
       <div className="container mb-5 grid grid-cols-1 md:grid-cols-5 grid-rows-10 bg-gradient-to-b from-gray-50 via-gray-100 to-gray-50 shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-2xl">
-        <div className="col-span-1 md:col-span-full justify-center m-2 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+        <div className="col-span-1 md:col-span-full justify-center m-2 ">
           <ImageGallery images={[data.site.image]} imageBlurhash={[data.site.imageBlurhash]} />
+          
         </div>
-        <div className="bg-white col-span-1 md:col-span-full m-2 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+        
+        <div className="col-span-1 md:col-span-full m-2 ">
           <DashHeader title={data.title} guests={propertyDetails.maxGuests} bedrooms={propertyDetails.totalBedrooms} bathrooms={propertyDetails.bathrooms} />
           <hr />
         </div>
-        <div className="relative bg-white col-span-1 md:col-span-2 md:min-w-[300px] m-2 row-span-4 row-start-3 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
-          <div className="sticky top-0 pt-[50px] pb-[50px]">
+        <div className="relative col-span-1 md:col-span-2 md:min-w-[300px] m-2 row-span-4 row-start-3 ">
+          <div className="sticky top-0 pt-[150px] pb-[150px]">
             <BookingComponent listing={data} />
           </div>
         </div>
-        <div className="bg-white rounded-sm col-start-1 md:col-start-3 md:col-span-3 p-8 m-2 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+        <div className="rounded-sm col-start-1 md:col-start-3 md:col-span-3 p-8 m-2 ">
           <div className="">
             Notable Features
           </div>
+          <hr className="mt-10"/>
         </div>
-        <div className="bg-white col-start-1 md:col-start-3 md:col-span-3 p-8 m-2 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+        <div className=" col-start-1 md:col-start-3 md:col-span-3 p-8 m-2 ">
           <div className="">
             Listing Description
           </div>
@@ -102,22 +105,26 @@ export default async function SitePostPage({
               <ShowMoreModal text={data.description} />
             </div>
           )}
+          <hr className="mt-10"/>
         </div>
-        <div className="bg-white col-start-1 md:col-start-3 md:col-span-3 p-8 m-2 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+        <div className=" col-start-1 md:col-start-3 md:col-span-3 p-8 m-2 mb-8">
           <div className="">
             Sleeping quarters and beds
           </div>
+          <hr className="mt-10"/>
         </div>
-        <div className="bg-white col-start-1 md:col-start-3 md:col-span-3 p-8 m-2 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+        <div className=" col-start-1 md:col-start-3 md:col-span-3 p-8 m-2 mb-8">
           <div className="flex justify-center">
             Amenities
             <AmenitiesModal amenityDetails={amenityDetails} />
           </div>
+          <hr className="mt-10"/>
         </div>
-        <div className="bg-white col-start-1 md:col-start-3 md:col-span-3 m-2 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+        <div className=" col-start-1 md:col-start-3 md:col-span-3 p-8 m-2 mb-8">
           <div className="w-full mx-auto flex justify-center">
             <CalendarDemo />
           </div>
+
         </div>
         <hr className="m-5 col-span-1 md:col-span-full" />
         <div className="col-span-1 md:col-span-full m-2">
