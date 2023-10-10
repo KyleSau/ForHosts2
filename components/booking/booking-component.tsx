ef0d440f9ad864b77aaf1cfe2e425dc6e67c7cb3
mcdoodle97 <mseamons5@hotmail.com>
2023-10-10 07:17:31 -0700

    site page changes

--- components/booking/booking-component.tsx
+++ components/booking/booking-component.tsx
@@ -83,7 +83,7 @@ const BookingComponent: React.FC<BookingProps> = ({ listing, className }: any) =
 
 
     return (
-        <div className="p-5 bg-white text-slate-600 rounded-sm items-center shadow-[0_3px_10px_rgb(0,0,0,0.2)] min-w-[350px] border border-slate-300 max-w-[375px] m-auto">
+        <div className="p-5 bg-white text-slate-600 rounded-sm items-center shadow-[0_3px_10px_rgb(0,0,0,0.2)] min-w-[350px] border border-slate-300 max-w-[375px] m-auto lg:block md:hidden">
             {/* {stripeInstance && (
                 <Elements stripe={stripeInstance}>
                     <CardInput />
