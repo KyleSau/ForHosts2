1d9a4838bc17d648cdac8cf52dd50e8ed286673e
Kyle Saunders <kwsaunders@pipeline.sbcc.edu>
2023-10-19 02:15:35 -0700

    fixed photo uploader

--- components/editor/photo-manager/photo-uploader.tsx
+++ components/editor/photo-manager/photo-uploader.tsx
@@ -1,24 +1,57 @@
 "use client"
 import { FC } from "react";
+import { Image } from 'lucide-react'
 
 interface UploaderProps {
     onFileUpload: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 const PhotoUploader: FC<UploaderProps> = ({ onFileUpload }) => {
+
+    const handleDragOver = (e: React.DragEvent<HTMLInputElement>) => {
+        e.preventDefault();
+    };
+
+    const handleDrop = (e: React.DragEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        if (e.dataTransfer.files) {
+            onFileUpload({
+                target: {
+                    files: e.dataTransfer.files
+                }
+            } as React.ChangeEvent<HTMLInputElement>);
+        }
+    };
+
     return (
-        <div className="uploader" key="photo-uploader">
-            <input
-                type="file"
-                multiple
-                accept="image/png, image/jpeg"
+        <div
+            id={""}
+            className="relative flex flex-col justify-center items-center text-center py-5 text-gray-400 border-dashed border-2 hover:border-solid border-black rounded"
+        >
+            <input accept="image/png, image/jpeg" type="file" title="" multiple
+                // className="absolute inset-0 z-50 w-full h-full p-0 m-0 outline-none opacity-0 cursor-pointer"
+                className="absolute inset-0 w-full h-full p-0 m-0 outline-none opacity-0 cursor-pointer"
+                onDrop={handleDrop}
+                onDragOver={handleDragOver}
                 onChange={onFileUpload}
-                className="file-input"
             />
-            <label htmlFor="file-input" className="file-label">
-                Click or drag files here to upload
-            </label>
+            <Image id="drag-drop-image-icon" />
+            <p className="m-0">Drag your files here or click in this area.</p>
         </div>
+        // <div className="uploader border-dashed hover:boder-solid border-2 border-black flex flex-col items-center justify-center" key="photo-uploader">
+        //     <input
+        //         type="file"
+        //         multiple
+        //         accept="image/png, image/jpeg"
+        //         onChange={onFileUpload}
+        //         className="file-input"
+        //     />
+        //     <label htmlFor="file-input" className="file-label text-center mt-2">
+        //         <span className="justify-center items-center"><Image size={32} className="text-black mb-1" /></span>
+        //         Click or drag files here to upload
+        //     </label>
+        // </div>
+
     );
 };
 
