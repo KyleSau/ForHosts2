9a80c470dc5bc6ba9c7b292f8f9348f52b7ab2da
mcdoodle97 <mseamons5@hotmail.com>
2023-10-19 07:19:16 -0700

    updats to photo editor css

--- components/editor/photo-manager/local-photo-card.tsx
+++ components/editor/photo-manager/local-photo-card.tsx
@@ -1,29 +1,31 @@
-import BlurImage from '@/components/blur-image'
-import { placeholderBlurhash } from '@/lib/utils'
-import { Loader2 } from 'lucide-react'
-import React from 'react'
-import { LocalPhoto } from './local-photo';
+import BlurImage from "@/components/blur-image";
+import { placeholderBlurhash } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
+import React from "react";
+import { LocalPhoto } from "./local-photo";
 
 interface LocalPhotoCardProps {
-    photo: LocalPhoto;
-    className?: string;  // Add this line to accept a className prop
+  photo: LocalPhoto;
+  className?: string; // Add this line to accept a className prop
 }
 
-export default function LocalPhotoCard
-    ({ photo, className }: LocalPhotoCardProps) {
-    return (
-        <div className={className} key={"non-draggable"}>
-            <button type="button" className="z-10 absolute left-2 top-2">
-                <Loader2 className="animate-spin" />
-            </button>
-            <BlurImage
-                alt={photo.url ?? ""}
-                blurDataURL={placeholderBlurhash}
-                className="object-fit h-[300px] w-full pb-1 filter grayscale"
-                width={200}
-                height={200}
-                src={photo.url ?? "/placeholder.png"}
-            />
-        </div>
-    )
+export default function LocalPhotoCard({
+  photo,
+  className,
+}: LocalPhotoCardProps) {
+  return (
+    <div className={className} key={"non-draggable"}>
+      <button type="button" className="absolute left-2 top-2 z-10">
+        <Loader2 className="animate-spin" />
+      </button>
+      <BlurImage
+        alt={photo.url ?? ""}
+        blurDataURL={placeholderBlurhash}
+        className=" object-fit  h-[300px] w-full pb-1 grayscale filter"
+        width={200}
+        height={200}
+        src={photo.url ?? "/placeholder.png"}
+      />
+    </div>
+  );
 }
--- components/editor/photo-manager/photo-manager.tsx
+++ components/editor/photo-manager/photo-manager.tsx
@@ -1,128 +1,162 @@
-"use client"
-import React, { useState } from 'react'
-import { ReactSortable, Sortable } from 'react-sortablejs';
-import PhotoUploader from './photo-uploader';
-import { Image } from '@prisma/client';
+"use client";
+import React, { useState } from "react";
+import { ReactSortable, Sortable } from "react-sortablejs";
+import PhotoUploader from "./photo-uploader";
+import { Image } from "@prisma/client";
 
 import { FILE_CONSTS, IMAGE_UPLOAD_QUANTITY_LIMIT } from "@/lib/constants";
-import PhotoCard from './photo-card';
-import { put } from '@vercel/blob';
-import { shiftBlobMetadata, createImageMetadata } from '@/lib/blob_actions';
-import LocalPhotoCard from './local-photo-card';
-import { LocalPhoto } from './local-photo';
-import { SplineIcon } from 'lucide-react';
+import PhotoCard from "./photo-card";
+import { put } from "@vercel/blob";
+import { shiftBlobMetadata, createImageMetadata } from "@/lib/blob_actions";
+import LocalPhotoCard from "./local-photo-card";
+import { LocalPhoto } from "./local-photo";
+import { SplineIcon } from "lucide-react";
 const PERMITTED_TYPES = [FILE_CONSTS.FILE, FILE_CONSTS.JPEG, FILE_CONSTS.PNG];
 
 const IMAGE_SIZE_LIMIT_MB = 30;
 const IMAGE_SIZE_LIMIT_BYTES = IMAGE_SIZE_LIMIT_MB * 1024 * 1024;
 
 interface PhotoMangerProps {
-    images: Image[];
-    postId: string;
-    siteId: string;
+  images: Image[];
+  postId: string;
+  siteId: string;
 }
 
-export default function PhotoManager({ images, postId, siteId }: PhotoMangerProps) {
+export default function PhotoManager({
+  images,
+  postId,
+  siteId,
+}: PhotoMangerProps) {
+  const [photos, setPhotos] = useState<Image[]>(images);
+  const [localPhotos, setLocalPhotos] = useState<LocalPhoto[]>([]);
 
-    const [photos, setPhotos] = useState<Image[]>(images);
-    const [localPhotos, setLocalPhotos] = useState<LocalPhoto[]>([]);
+  const handleDelete = (index: number) => {
+    setPhotos((prevPhotos) => prevPhotos.filter((_, i) => i !== index));
+  };
 
-    const handleDelete = (index: number) => {
-        setPhotos(prevPhotos => prevPhotos.filter((_, i) => i !== index));
-    };
+  const onPhotoDragEnd = async (event: Sortable.SortableEvent) => {
+    const { oldIndex, newIndex } = event;
+    if (oldIndex === undefined || newIndex === undefined) return;
+    shiftBlobMetadata(postId, oldIndex, newIndex);
+  };
 
-    const onPhotoDragEnd = async (event: Sortable.SortableEvent) => {
-        const { oldIndex, newIndex } = event;
-        if (oldIndex === undefined || newIndex === undefined)
-            return;
-        shiftBlobMetadata(postId, oldIndex, newIndex);
+  const movePhoto = (oldIndex: number, newIndex: number) => {
+    setPhotos((prevPhotos) => {
+      const updatedPhotos = [...prevPhotos];
+      const [movedPhoto] = updatedPhotos.splice(oldIndex, 1);
+      updatedPhotos.splice(newIndex, 0, movedPhoto);
+      return updatedPhotos;
+    });
+  };
+
+  const onPhotoUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = event.target;
+    const localFiles = Array.from(files ?? []).filter((file) =>
+      PERMITTED_TYPES.includes(file.type),
+    );
+
+    if (
+      photos.length > IMAGE_UPLOAD_QUANTITY_LIMIT ||
+      photos.length + localFiles.length > IMAGE_UPLOAD_QUANTITY_LIMIT
+    ) {
+      alert(
+        `Only ${IMAGE_UPLOAD_QUANTITY_LIMIT} images may be uploaded for this listing`,
+      );
+      return;
     }
 
-    const movePhoto = (oldIndex: number, newIndex: number) => {
-        setPhotos(prevPhotos => {
-            const updatedPhotos = [...prevPhotos];
-            const [movedPhoto] = updatedPhotos.splice(oldIndex, 1);
-            updatedPhotos.splice(newIndex, 0, movedPhoto);
-            return updatedPhotos;
+    setLocalPhotos((prevLocalPhotos) => [
+      ...prevLocalPhotos,
+      ...localFiles.map((file) => ({
+        url: URL.createObjectURL(file),
+        name: file.name,
+      })),
+    ]);
+
+    uploadPhotos(localFiles);
+  };
+
+  const uploadPhotos = async (localFiles: File[]) => {
+    let oversizedFileNames: string[] = [];
+
+    for (let i = 0; i < localFiles.length; i++) {
+      // Use a traditional for loop to process files one by one
+      const file = localFiles[i];
+
+      if (file.size > IMAGE_SIZE_LIMIT_BYTES) {
+        oversizedFileNames.push(file.name);
+        continue;
+      }
+
+      try {
+        const blobResult = await put(file.name, file, {
+          access: "public",
+          handleBlobUploadUrl: "/api/upload",
         });
+
+        const image = await createImageMetadata(blobResult, postId, siteId);
+
+        if (image) {
+          setPhotos((prevPhotos) => [...prevPhotos, image]);
+
+          // Remove the corresponding localPhoto
+          setLocalPhotos((prevLocalPhotos) =>
+            prevLocalPhotos.filter(
+              (localPhoto) => localPhoto.name !== file.name,
+            ),
+          );
+        }
+      } catch (error) {
+        console.error("Error uploading file:", error);
+      }
     }
 
-    const onPhotoUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const { files } = event.target;
-        const localFiles = Array.from(files ?? []).filter(file => PERMITTED_TYPES.includes(file.type));
-
-        if (photos.length > IMAGE_UPLOAD_QUANTITY_LIMIT || photos.length + localFiles.length > IMAGE_UPLOAD_QUANTITY_LIMIT) {
-            alert(`Only ${IMAGE_UPLOAD_QUANTITY_LIMIT} images may be uploaded for this listing`);
-            return;
-        }
-
-        setLocalPhotos(prevLocalPhotos => [
-            ...prevLocalPhotos,
-            ...localFiles.map(file => ({ url: URL.createObjectURL(file), name: file.name }))
-        ]);
-
-        uploadPhotos(localFiles);
+    // Remove all oversized files from localPhotos at once
+    if (oversizedFileNames.length > 0) {
+      alert(
+        `${JSON.stringify(oversizedFileNames)} ${
+          oversizedFileNames.length > 1 ? "were" : "was"
+        } not uploaded because it exceeds the file size limit of ${IMAGE_SIZE_LIMIT_MB} MB.`,
+      );
+      setLocalPhotos((prevLocalPhotos) =>
+        prevLocalPhotos.filter(
+          (localPhoto) => !oversizedFileNames.includes(localPhoto.name),
+        ),
+      );
     }
+  };
 
-    const uploadPhotos = async (localFiles: File[]) => {
-        let oversizedFileNames: string[] = [];
+  return (
+    <div>
+      <ReactSortable
+        className="grid gap-2 transition-all duration-500 ease-in sm:grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3"
+        list={photos}
+        setList={setPhotos}
+        onEnd={onPhotoDragEnd}
+        filter=".non-draggable"
+        preventOnFilter={false}
+      >
+        {photos.map((photo: Image, index: number) => (
+          <PhotoCard
+            handleDelete={handleDelete}
+            movePhoto={movePhoto}
+            postId={postId}
+            key={photo.orderIndex}
+            index={index}
+            photo={photo}
+            totalImages={photos.length}
+          />
+        ))}
+        {localPhotos.map((photo: LocalPhoto) => (
+          <LocalPhotoCard
+            key={photo.name}
+            photo={photo}
+            className="non-draggable relative w-[375px]"
+          />
+        ))}
 
-        for (let i = 0; i < localFiles.length; i++) {  // Use a traditional for loop to process files one by one
-            const file = localFiles[i];
-
-            if (file.size > IMAGE_SIZE_LIMIT_BYTES) {
-                oversizedFileNames.push(file.name);
-                continue;
-            }
-
-            try {
-                const blobResult = await put(file.name, file, {
-                    access: 'public',
-                    handleBlobUploadUrl: '/api/upload'
-                });
-
-                const image = await createImageMetadata(blobResult, postId, siteId);
-
-                if (image) {
-                    setPhotos(prevPhotos => [...prevPhotos, image]);
-
-                    // Remove the corresponding localPhoto
-                    setLocalPhotos(prevLocalPhotos =>
-                        prevLocalPhotos.filter(localPhoto => localPhoto.name !== file.name)
-                    );
-                }
-            } catch (error) {
-                console.error('Error uploading file:', error);
-            }
-        }
-
-        // Remove all oversized files from localPhotos at once
-        if (oversizedFileNames.length > 0) {
-            alert(`${JSON.stringify(oversizedFileNames)} ${oversizedFileNames.length > 1 ? 'were' : 'was'} not uploaded because it exceeds the file size limit of ${IMAGE_SIZE_LIMIT_MB} MB.`);
-            setLocalPhotos(prevLocalPhotos =>
-                prevLocalPhotos.filter(localPhoto => !oversizedFileNames.includes(localPhoto.name))
-            );
-        }
-    };
-
-    return (
-        <div>
-            <ReactSortable
-                className="grid grid-cols-1 gap-2 md:grid-cols-2 2xl:grid-cols-3 grid-rows-[300px] md:grid-rows-[300px] 2xl:grid-rows-[300px]"
-                list={photos}
-                setList={setPhotos}
-                onEnd={onPhotoDragEnd}
-                filter=".non-draggable"
-                preventOnFilter={false}
-            >
-                {photos.map((photo: Image, index: number) => (
-                    <PhotoCard handleDelete={handleDelete} movePhoto={movePhoto} postId={postId} key={photo.orderIndex} index={index} photo={photo} totalImages={photos.length} />
-                ))}
-                {localPhotos.map((photo: LocalPhoto) => (
-                    <LocalPhotoCard key={photo.name} photo={photo} className="relative non-draggable" />
-                ))}
-                <PhotoUploader onFileUpload={onPhotoUpload} />
-            </ReactSortable>
-        </div>
-    )
+        <PhotoUploader onFileUpload={onPhotoUpload} />
+      </ReactSortable>
+    </div>
+  );
 }
