bcf2fafa5b4259cb03af6c46da2c91f03f9fa705
mcxu <michaelxu79@gmail.com>
2023-10-20 16:14:08 -0400

    Changed FILE to BMP for permitted types in photo manager.

--- components/editor/photo-manager/photo-manager.tsx
+++ components/editor/photo-manager/photo-manager.tsx
@@ -19,7 +19,7 @@ import EditorWrapper from "../editor-wrapper";
 import EditorTitle from "../editor-components-title";
 
 const PERMITTED_TYPES = new Set([
-    FILE_CONSTS.FILE,
+    FILE_CONSTS.BMP,
     FILE_CONSTS.JPEG,
     FILE_CONSTS.PNG,
 ]);
