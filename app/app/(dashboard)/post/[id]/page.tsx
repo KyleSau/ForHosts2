e1528d745ced6864acf0804b6da4113a12e7736f
Kyle Saunders <kwsaunders@pipeline.sbcc.edu>
2023-10-23 10:20:38 -0700

    asdfa

--- app/app/(dashboard)/post/[id]/page.tsx
+++ app/app/(dashboard)/post/[id]/page.tsx
@@ -31,5 +31,6 @@ export default async function PostPage({ params }: { params: { id: string } }) {
 
   return (
     <PhotoManager images={sortedImages} postId={data.id} siteId={data.siteId!} />
+    // kekker
   );
 }
