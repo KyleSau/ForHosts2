096875364676d1c1d26451bc8416294537e97e1e
mcxu <michaelxu79@gmail.com>
2023-10-30 00:24:19 -0400

    another save point

--- app/app/admin-dashboard/page.tsx
+++ app/app/admin-dashboard/page.tsx
@@ -8,46 +8,46 @@ import { RollerCoaster } from 'lucide-react';
 import { User, Role } from "@prisma/client";
 import HostTable from './host-table';
 import ReservationTable from './reservation-table';
-import { DataTable } from './host-table-shadcn';
-import { ColumnDef } from '@tanstack/react-table';
+import { DataTable } from './data-table-shadcn';
+import { Payment, tableTestColumns } from './columns-shadcn';
+// import { ColumnDef } from '@tanstack/react-table';
+
 
 export const dynamic = 'force-dynamic';
 
-//Shadcn example columns
-type Payment = {
-    id: string
-    amount: number
-    status: "pending" | "processing" | "success" | "failed"
-    email: string
-}
-
-export const columns: ColumnDef<Payment>[] = [
-    {
-        accessorKey: "status",
-        header: "Status",
-    },
-    {
-        accessorKey: "email",
-        header: "Email",
-    },
-    {
-        accessorKey: "amount",
-        header: "Amount",
-    },
-]
-
 //For Test (https://ui.shadcn.com/docs/components/data-table#filtering)
+// const paymentsData: Payment[] = [
+//     {
+//         id: "728ed52f",
+//         amount: 100,
+//         status: "pending",
+//         email: "m@example.com",
+//     },
+//     {
+//         id: "489e1d42",
+//         amount: 125,
+//         status: "processing",
+//         email: "example@gmail.com",
+//     }
+// ]
+
 async function getData(): Promise<Payment[]> {
     // Fetch data from your API here.
     return [
-      {
-        id: "728ed52f",
-        amount: 100,
-        status: "pending",
-        email: "m@example.com",
-      }
-    ]
-  }
+        {
+            id: "728ed52f",
+            amount: 100,
+            status: "pending",
+            email: "m@example.com",
+        },
+        {
+            id: "489e1d42",
+            amount: 125,
+            status: "processing",
+            email: "example@gmail.com",
+        }
+    ];
+}
 
 
 export default async function IndexPage({
@@ -55,51 +55,52 @@ export default async function IndexPage({
 }: {
     searchParams: { q: string };
 }) {
-    const search = searchParams.q ?? '';
-    console.log("search: ", search);
-    const sites = await prisma?.site.findMany();
-    console.log("sites: ", sites);
-    const listings = await prisma?.post.findMany();
-    console.log("listings: ", listings);
-    const reservations = await prisma?.reservation.findMany();
-    console.log("reservations: ", reservations);
-    const users: User[] | undefined = await prisma?.user.findMany({
-        where: {
-            name: {
-                contains: search,
-                mode: "insensitive" // This will make the search case-insensitive
-            }
-        },
-        orderBy: {
-            createdAt: 'desc'
-        },
-        include: {
-            sites: true,
-            posts: true
-        }
-    });
-    console.log("users: ", users);
+    // const search = searchParams.q ?? '';
+    // console.log("search: ", search);
+    // const sites = await prisma?.site.findMany();
+    // console.log("sites: ", sites);
+    // const listings = await prisma?.post.findMany();
+    // console.log("listings: ", listings);
+    // const reservations = await prisma?.reservation.findMany();
+    // console.log("reservations: ", reservations);
+    // const users: User[] | undefined = await prisma?.user.findMany({
+    //     where: {
+    //         name: {
+    //             contains: search,
+    //             mode: "insensitive" // This will make the search case-insensitive
+    //         }
+    //     },
+    //     orderBy: {
+    //         createdAt: 'desc'
+    //     },
+    //     include: {
+    //         sites: true,
+    //         posts: true
+    //     }
+    // });
+    // console.log("users: ", users);
 
-    const data = [
-        {
-            category: 'Websites',
-            stat: sites?.length,
-        },
-        {
-            category: 'Listings',
-            stat: listings?.length,
-        },
-        {
-            category: 'Reservations',
-            stat: reservations?.length,
-        }
-    ];
+    // const data = [
+    //     {
+    //         category: 'Websites',
+    //         stat: sites?.length,
+    //     },
+    //     {
+    //         category: 'Listings',
+    //         stat: listings?.length,
+    //     },
+    //     {
+    //         category: 'Reservations',
+    //         stat: reservations?.length,
+    //     }
+    // ];
 
     const tableTestData = await getData();
+    console.log("tableTestColumns: ", tableTestColumns);
 
     return (
         <main className="p-4 md:p-10 mx-auto max-w-7xl">
-            <Grid numItemsSm={2} numItemsLg={3} className="gap-6 mb-4">
+            {/* <Grid numItemsSm={2} numItemsLg={3} className="gap-6 mb-4">
                 {data.map((item) => (
                     <Card key={item.category}>
                         <Title>{item.category}</Title>
@@ -113,9 +114,9 @@ export default async function IndexPage({
                         </Flex>
                     </Card>
                 ))}
-            </Grid>
+            </Grid> */}
 
-            <DataTable columns={columns} data={tableTestData} />
+            <DataTable columns={tableTestColumns} data={tableTestData} />
             
             {/* <Title>Recent Reservations</Title>
             <Searchable />
