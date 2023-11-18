1ec393c7c49cb08156073b7e966c2686c32a513f
mcdoodle97 <mseamons5@hotmail.com>
2023-11-18 15:35:49 -0800

    blog responsive design listing page

--- components/blog-listing-page/main-blog-cards.tsx
+++ components/blog-listing-page/main-blog-cards.tsx
@@ -13,53 +13,46 @@ const MainBlogCard: React.FC<BlogCardProps> = ({ blog, isVisible }) => {
       }`}
     >
       <div className="space-y-2">
-        <article className="rounded-lg bg-white p-4 shadow-lg">
-          <div className="flex items-start space-x-4 ">
-            <Image
-              alt="Blog 1 image"
-              className="rounded-md"
-              height="100"
-              src={blog.image}
-              style={{
-                aspectRatio: "100/100",
-                objectFit: "cover",
-              }}
-              width="100"
-            />
+        <Link href={`/blogs/${blog.slug}`}>
+          <article className="  rounded-lg bg-white p-4 shadow-lg ">
+            <div className="flex items-start space-x-4 ">
+              <Image
+                alt="Blog 1 image"
+                className="max-w-[150px] rounded-md md:max-w-[275px]"
+                height="200"
+                src={blog.image}
+                style={{
+                  aspectRatio: "100/100",
+                  objectFit: "cover",
+                }}
+                width="300"
+              />
 
-            <div className="truncate">
-              <h2 className=" text-2xl font-semibold">{blog.title}</h2>
-              <div className="text-gray-500 dark:text-gray-400">
-                <span className="flex items-center">
-                  <div className="mr-1 flex h-8 w-8 items-center justify-center border-0 text-gray-500">
-                    <Image
-                      alt={blog.author}
-                      className=" rounded-full"
-                      height="24"
-                      title={blog.author}
-                      src={blog.avatar}
-                      width="24"
-                    />
-                  </div>
-                  Posted on November 11, 2023
-                </span>
+              <div className="md:truncate">
+                <h2 className=" text-2xl font-semibold">{blog.title}</h2>
+                <div className="text-gray-500 dark:text-gray-400">
+                  <span className="flex items-center">
+                    <div className="mr-1 flex h-8 w-8 items-center justify-center border-0 text-gray-500">
+                      <Image
+                        alt={blog.author}
+                        className=" rounded-full"
+                        height="24"
+                        title={blog.author}
+                        src={blog.avatar}
+                        width="24"
+                      />
+                    </div>
+                    Posted on November 11, 2023
+                  </span>
+                </div>
+                <p className="text-gray-700 dark:text-gray-300">
+                  {blog.description}
+                </p>
               </div>
-              <p className="text-gray-700 dark:text-gray-300">
-                {blog.description}
-              </p>
             </div>
-          </div>
-          <div className="mt-4">
-            <Link href={`/blogs/${blog.slug}`}>
-              <Button
-                variant="outline"
-                className="border border-gray-300 hover:scale-105 hover:font-bold"
-              >
-                Read More
-              </Button>
-            </Link>
-          </div>
-        </article>
+            <div className="mt-4"></div>
+          </article>
+        </Link>
       </div>
     </div>
   );
--- components/blog-listing-page/recent-blog-card.tsx
+++ components/blog-listing-page/recent-blog-card.tsx
@@ -1,31 +1,33 @@
 import React from "react";
 import { BlogCardProps } from "./types";
 import Image from "next/image";
-
+import Link from "next/link";
 const RecentBlogCard: React.FC<BlogCardProps> = ({ blog }) => {
   return (
     <div className="mx-2 my-2 w-full cursor-pointer text-start">
-      <ul className="divide-y divide-gray-200 dark:divide-gray-800">
-        <li className="flex items-start space-x-4 py-4">
-          <Image
-            alt="Blog 3 image"
-            className="rounded-md"
-            height="50"
-            src="/forhoststestlogo.png"
-            style={{
-              aspectRatio: "50/50",
-              objectFit: "cover",
-            }}
-            width="50"
-          />
-          <div>
-            <h2 className="text-xl font-semibold">{blog.title}</h2>
-            <p className="text-gray-500 dark:text-gray-400">
-              Posted on November 9, 2023
-            </p>
-          </div>
-        </li>
-      </ul>
+      <Link href={`/blogs/${blog.slug}`}>
+        <ul className="divide-y divide-gray-200 dark:divide-gray-800">
+          <li className="flex items-start space-x-4 py-4">
+            <Image
+              alt="Blog 3 image"
+              className="rounded-md"
+              height="50"
+              src="/forhoststestlogo.png"
+              style={{
+                aspectRatio: "50/50",
+                objectFit: "cover",
+              }}
+              width="50"
+            />
+            <div>
+              <h2 className="text-xl font-semibold">{blog.title}</h2>
+              <p className="text-gray-500 dark:text-gray-400">
+                Posted on November 9, 2023
+              </p>
+            </div>
+          </li>
+        </ul>
+      </Link>
       <hr />
     </div>
   );
