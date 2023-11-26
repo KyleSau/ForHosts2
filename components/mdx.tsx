f39a7a385a980e4355e696d0067a49f99bb1997b
Kyle Saunders <kwsaunders@pipeline.sbcc.edu>
2023-11-25 18:08:00 -0800

    fuckit

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -16,7 +16,7 @@ export default function MDX({ source }: { source: MDXRemoteProps }) {
 
   return (
     <article
-      className="items-center prose-md prose prose-stone m-auto w-11/12 dark:prose-invert sm:prose-lg sm:w-3/4"
+      className="item-center prose-md prose prose-stone m-auto w-11/12 dark:prose-invert sm:prose-lg sm:w-3/4"
       suppressHydrationWarning={true}
     >
       {/* @ts-ignore */}
