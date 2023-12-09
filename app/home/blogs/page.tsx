8e723799681db8f69cf17f50b5034b42fa424390
mcdoodle97 <mseamons5@hotmail.com>
2023-12-09 15:49:39 -0800

    dfds

--- app/home/blogs/page.tsx
+++ app/home/blogs/page.tsx
@@ -1,7 +1,7 @@
 import { getAllBlogs } from "@/lib/actions";
 import MainBlogComponent from "../../../components/blog-listing-page/presenting-blog-component";
 export default async function BlogPage() {
-  const blogs = await prisma?.blog.findMany();
+  const blogs = await getAllBlogs();
   return (
     <div className="bg-gray-50">
       <MainBlogComponent blogs={blogs} />
