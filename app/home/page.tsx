271045dcc8a83670937fe0348b95aec0ed78f44e
Kyle Saunders <kwsaund@gmail.com>
2024-05-01 19:39:25 -0700

    removed unused imports

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,4 @@
 // page.tsx
-import HomeLayout from "@/components/home/home-layout";
-import { Hero } from "./hero";
-import { Benefits } from "./benefits";
-import { HowItWorks } from "./HowItWorks";
-import { Testimonials } from "./Testimonials";
-import { Footer } from "react-day-picker";
 import Home from "./Home";
 
 export default function HomePage() {
